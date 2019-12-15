b7bfcd43e9bb2ab8f84626ea1e261b87694df8c8
Yavgeni Hotimsky <yavgeni.hotimsky@databand.ai>
2019-12-15 15:31:08 +0200

    heartbeat sender now uses tracking store (and works)

--- modules/dbnd/src/dbnd/_core/cli/cmd_heartbeat.py
+++ modules/dbnd/src/dbnd/_core/cli/cmd_heartbeat.py
@@ -1,11 +1,27 @@
-from dbnd._core.task_executor.heartbeat_sender import send_heartbeat_continuously
 from dbnd._vendor import click
 from dbnd._vendor.click import command
+from dbnd._vendor.click.types import IntParamType
 
 
 @command()
 @click.option("--run-uid", required=True)
 @click.option("--tracking-url", required=True)
-@click.option("--heartbeat-interval", required=True)
-def send_heartbeat(run_uid, tracking_url, heartbeat_interval):
-    send_heartbeat_continuously(run_uid, tracking_url, heartbeat_interval)
+@click.option("--heartbeat-interval", required=True, type=IntParamType())
+@click.option("--tracker", required=True)
+@click.option("--tracker-api", required=True)
+def send_heartbeat(run_uid, tracking_url, heartbeat_interval, tracker, tracker_api):
+    from dbnd import config
+    from dbnd._core.settings import CoreConfig
+    from dbnd._core.task_executor.heartbeat_sender import send_heartbeat_continuously
+
+    with config(
+        {
+            "core": {
+                "tracker": tracker.split(","),
+                "tracker_api": tracker_api,
+                "tracking_url": tracking_url,
+            }
+        }
+    ):
+        tracking_store = CoreConfig().get_tracking_store()
+        send_heartbeat_continuously(run_uid, tracking_store, heartbeat_interval)
--- modules/dbnd/src/dbnd/_core/task_executor/heartbeat_sender.py
+++ modules/dbnd/src/dbnd/_core/task_executor/heartbeat_sender.py
@@ -6,8 +6,8 @@ import sys
 
 from time import sleep, time
 
+from dbnd._core.tracking.tracking_store import TrackingStore
 from dbnd._core.utils.basics.format_exception import format_exception_as_str
-from dbnd.api.api_utils import ApiClient
 
 
 logger = logging.getLogger(__name__)
@@ -17,20 +17,25 @@ logger = logging.getLogger(__name__)
 def start_heartbeat_sender(run):
     from dbnd import config
 
-    heartbeat_interval_s = config.getint("task", "heartbeat_interval_s")
-    if heartbeat_interval_s > 0 and config.get("core", "tracker_api") == "web":
+    heartbeat_interval_s = config.getint("run", "heartbeat_interval_s")
+    if heartbeat_interval_s > 0:
         sp = None
         try:
+            core = run.context.settings.core
             sp = subprocess.Popen(
                 [
                     "dbnd",
-                    "send_heartbeat",
+                    "send-heartbeat",
                     "--run-uid",
                     str(run.run_uid),
                     "--tracking-url",
-                    config.get("core", "tracker_url"),
+                    core.tracker_url,
                     "--heartbeat-interval",
-                    heartbeat_interval_s,
+                    str(heartbeat_interval_s),
+                    "--tracker",
+                    ",".join(core.tracker),
+                    "--tracker-api",
+                    core.tracker_api,
                 ]
             )
             yield
@@ -39,20 +44,22 @@ def start_heartbeat_sender(run):
                 sp.terminate()
     else:
         logger.info(
-            "run heartbeat sender disabled (set task.heartbeat_interval_s to value > 0 and core.tracker_api to web)"
+            "run heartbeat sender disabled (set task.heartbeat_interval_s to value > 0)"
         )
         yield
 
 
-def send_heartbeat_continuously(run_uid, tracking_url, heartbeat_interval_s):
+def send_heartbeat_continuously(
+    run_uid, tracking_store, heartbeat_interval_s
+):  # type: (str, TrackingStore, int) -> None
     logger.info(
         "starting heartbeat sender process (pid %s) with a send interval of %s seconds"
         % (os.getpid(), heartbeat_interval_s)
     )
 
     parent_pid = os.getppid()
-    api_client = ApiClient(tracking_url)
-    payload = {"run_uid": run_uid}
+    # api_client = ApiClient(tracking_url)
+    # payload = {"run_uid": run_uid}
     try:
         while True:
             loop_start = time()
@@ -62,7 +69,8 @@ def send_heartbeat_continuously(run_uid, tracking_url, heartbeat_interval_s):
                 except ProcessLookupError:
                     return
 
-                api_client.api_request("heartbeat", payload)
+                # api_client.api_request("heartbeat", payload)
+                tracking_store.heartbeat(run_uid=run_uid)
             except KeyboardInterrupt:
                 logger.info("stopping heartbeat sender process due to interrupt")
                 return
