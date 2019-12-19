e9aa5df65d6310098fc435941fee65b7e1dea72f
Evgeny Shulman <evgeny.shulman@databand.ai>
2019-12-19 10:11:32 +0200

    test for config as task parameter

---
+++ modules/dbnd/test_dbnd/task/parameters/test_parameter_as_config_object.py
@@ -0,0 +1,37 @@
+import pytest
+
+from dbnd import Config, config, parameter, task
+from dbnd._core.errors import TaskClassNotFoundException, DatabandBuildError
+from dbnd._core.task_build.task_registry import get_task_registry
+
+
+class TConfig(Config):
+    c_value = parameter[str]
+
+
+@task(t_config=parameter[TConfig])
+def t_with_config(t_config):
+    # type: (TConfig)-> str
+    return t_config.c_value
+
+
+class TestParametersAsConfigObjects(object):
+    def test_can_build(self):
+        with config({"t_config": {"_type": "TConfig", "c_value": "value"}}):
+            expected = t_with_config.task("t_config")
+            expected.dbnd_run()
+
+    def test_can_not_find(self):
+        with pytest.raises(TaskClassNotFoundException):
+            get_task_registry().get_task_cls("t_config")
+
+    def test_can_not_build_not_found(self):
+        with pytest.raises(DatabandBuildError):
+            expected = t_with_config.task("t_config")
+            expected.dbnd_run()
+
+    def test_can_not_build_no_value(self):
+        with pytest.raises(DatabandBuildError):
+            with config({"t_config": {"_type": "TConfig"}}):
+                expected = t_with_config.task("t_config")
+                expected.dbnd_run()
