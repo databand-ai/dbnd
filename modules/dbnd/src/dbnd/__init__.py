1a9a2472f96deb9dc7a0aeadaa7e694f7a6d7290
Evgeny Shulman <evgeny.shulman@databand.ai>
2019-12-29 14:58:30 +0200

    Bump version: 0.24.8 → 0.24.9

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -116,7 +116,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.24.8"
+__version__ = "0.24.9"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
