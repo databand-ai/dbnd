44b19b8358925e0f0862b95ae0d906e26b5b32d6
Evgeny Shulman <evgeny.shulman@databand.ai>
2020-01-13 22:41:27 +0200

    organizing scheduler_for_profiling

--- modules/dbnd-airflow/test_dbnd_airflow/scenarios/scheduler_for_profiling.py
+++ modules/dbnd-airflow/test_dbnd_airflow/scenarios/scheduler_for_profiling.py
@@ -1,44 +1,60 @@
 import logging
 import os
 
-from airflow import conf
-from airflow.jobs.scheduler_job import SchedulerJob
-from airflow.models import DagBag
-
-from dbnd import dbnd_bootstrap
-from dbnd._core.log.logging_utils import create_file_handler
-from dbnd_airflow.airflow_extensions.airflow_config import reinit_airflow_sql_conn
-from dbnd_airflow.executors.simple_executor import InProcessExecutor
-from test_dbnd_airflow.scenarios.scheduler_perf_experiment import (
-    dag_folder,
-    dag_id,
-    log_scheduler,
-)
-
 
 # before we load any config!
 os.environ["DBND_DISABLE_SCHEDULED_DAGS_LOAD"] = "True"
 os.environ["DBND__LOG__SQLALCHEMY_PROFILE"] = "True"
 os.environ["DBND__LOG__SQLALCHEMY_TRACE"] = "True"
+print(os.environ["DBND__LOG__SQLALCHEMY_TRACE"])
 
 
-dbnd_bootstrap()
+# in order to profile scheduler.py Dag Processor you need to add folowing lines to scheduler_job.py:145
+# # ========
+# # DBND
+# from dbnd_airflow.airflow_extensions.airflow_config import reinit_airflow_sql_conn
+#
+# reinit_airflow_sql_conn()
+#
+# from dbnd._core.log.logging_utils import create_file_handler
+# from test_dbnd_airflow.scenarios.scheduler_perf_experiment import log_processor_file
+#
+# logging.getLogger("airflow.processor").addHandler(create_file_handler(log_file=log_processor_file))
+# # ========
 
 
-reinit_airflow_sql_conn()
+def main():
+    from airflow import conf
+    from airflow.jobs.scheduler_job import SchedulerJob
+    from airflow.models import DagBag
 
-conf.set("core", "unit_test_mode", "True")
-logger = logging.getLogger(__name__)
+    from dbnd import dbnd_bootstrap
+    from dbnd._core.log.logging_utils import create_file_handler
+    from dbnd_airflow.airflow_extensions.airflow_config import reinit_airflow_sql_conn
+    from dbnd_airflow.executors.simple_executor import InProcessExecutor
+    from test_dbnd_airflow.scenarios.scheduler_perf_experiment import (
+        dag_folder,
+        dag_id,
+        log_scheduler,
+    )
 
-logging.root.addHandler(create_file_handler(log_file=log_scheduler))
+    dbnd_bootstrap()
+    reinit_airflow_sql_conn()
+    conf.set("core", "unit_test_mode", "True")
 
-dag_bag = DagBag(dag_folder=dag_folder)
-scheduler_job = SchedulerJob(
-    dag_ids=[dag_id],
-    subdir=dag_folder,
-    do_pickle=False,
-    num_runs=3,
-    executor=InProcessExecutor(dag_bag=dag_bag),
-)
+    logging.root.addHandler(create_file_handler(log_file=log_scheduler))
 
-scheduler_job.run()
+    dag_bag = DagBag(dag_folder=dag_folder)
+    scheduler_job = SchedulerJob(
+        dag_ids=[dag_id],
+        subdir=dag_folder,
+        do_pickle=False,
+        num_runs=3,
+        executor=InProcessExecutor(dag_bag=dag_bag),
+    )
+
+    scheduler_job.run()
+
+
+if __name__ == "__main__":
+    main()
