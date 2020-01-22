7d8e73564463e8ec5f938d5c4bd5264aa827aa2c
Oleksandr Hrab <oleksandr.hrab@databand.ai>
2020-01-22 19:30:26 +0000

    Feature/ohrab/windows tests skipping windows non-compatible tests (calling external process via subprocess)

--- modules/dbnd/test_dbnd/run/test_resubmit.py
+++ modules/dbnd/test_dbnd/run/test_resubmit.py
@@ -1,14 +1,18 @@
 from dbnd.testing.helpers import run_dbnd_subprocess__dbnd_run
+from dbnd.testing.helpers_pytest import skip_on_windows
 from test_dbnd.run.utils import DbndCmdTest
 
 
 class TestResubmit(DbndCmdTest):
+    @skip_on_windows
     def test_external_task_cmd_line(self):
         run_dbnd_subprocess__dbnd_run(["dbnd_sanity_check", "--env", "local_resubmit"])
 
+    @skip_on_windows
     def test_submit_can_run(self):
         self.dbnd_run_task_with_output(["--env", "local_resubmit"])
 
+    @skip_on_windows
     def test_submit_with_interactive(self):
         self.dbnd_run_task_with_output(["--env", "local_resubmit", "--interactive"])
 
