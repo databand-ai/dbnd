be13e19c263f3302a89ea5c005b9045fbba9b0b4
Yavgeni Hotimsky <yavgeni.hotimsky@databand.ai>
2020-02-27 18:50:18 +0200

    fixed azure credentials config

--- plugins/dbnd-azure/src/dbnd_azure/credentials.py
+++ plugins/dbnd-azure/src/dbnd_azure/credentials.py
@@ -1,9 +1,8 @@
 import logging
 
 from cachetools import cached
-from dbnd import Config, parameter
 from dbnd._core.plugin.dbnd_plugins import use_airflow_connections
-from dbnd_azure.env import AzureCloudConfig
+from dbnd_azure.env import AzureCredentialsConfig
 
 
 logger = logging.getLogger(__name__)
@@ -24,21 +23,4 @@ def get_azure_credentials():
         return aws_storage_credentials.get_credentials()
     else:
         logger.debug("getting azure credentials from dbnd config")
-        return AzureCloudConfig().simple_params_dict()
-
-
-class AzureCredentialsConfig(Config):
-    _conf__task_family = "azure_credentials"
-
-    account_name = parameter(
-        description="""The storage account name. This is used to authenticate requests
-                signed with an account key and to construct the storage endpoint. It
-                is required unless a connection string is given, or if a custom
-                domain is used with anonymous authentication."""
-    ).none()[str]
-    account_key = parameter(
-        description="""The storage account key. This is used for shared key authentication.
-                If neither account key or sas token is specified, anonymous access
-                will be used.""",
-        default=None,
-    ).none()[str]
+        return AzureCredentialsConfig().simple_params_dict()
