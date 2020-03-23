91f9c0eea452a4e0a75b987e9ca96d3cf90c0d17
Evgeny Shulman <evgeny.shulman@databand.ai>
2020-03-23 23:03:52 +0200

    extracing code into dbnd_spark_bootstrap

--- plugins/dbnd-spark/src/dbnd_spark/_plugin.py
+++ plugins/dbnd-spark/src/dbnd_spark/_plugin.py
@@ -7,9 +7,9 @@ from dbnd_spark.livy.livy_spark_config import LivySparkConfig
 @dbnd.hookimpl
 def dbnd_setup_plugin():
     from dbnd_spark.local.local_spark_config import SparkLocalEngineConfig
-    from dbnd_spark.targets import dbnd_register_spark_types
+    from dbnd_spark.spark_bootstrap import dbnd_spark_bootstrap
 
     register_config_cls(SparkLocalEngineConfig)
     register_config_cls(LivySparkConfig)
 
-    dbnd_register_spark_types()
+    dbnd_spark_bootstrap()
