35fe4db4cbae1490b4872dc2429a524758c19ca2
Evgeny Shulman <evgeny.shulman@databand.ai>
2020-03-23 16:32:49 +0200

    autoimporting spark types

--- plugins/dbnd-spark/src/dbnd_spark/__init__.py
+++ plugins/dbnd-spark/src/dbnd_spark/__init__.py
@@ -1,2 +1,6 @@
+from dbnd_spark.spark import SparkTask, PySparkTask, spark_task
 from dbnd_spark.spark_config import SparkConfig
 from dbnd_spark.spark_session import get_spark_session
+from dbnd_spark.targets import dbnd_register_spark_types
+
+dbnd_register_spark_types()
--- plugins/dbnd-spark/src/dbnd_spark/targets/__init__.py
+++ plugins/dbnd-spark/src/dbnd_spark/targets/__init__.py
@@ -1,19 +1,37 @@
-import pyspark
+import logging
+
+from dbnd._core.utils.seven import import_errors
 from dbnd_spark.targets.spark_marshalling import SparkMarshaller, SparkDataFrameToCsv
 from dbnd_spark.targets.spark_values import SparkDataFrameValueType
-
 from targets.marshalling import register_marshaller
 from targets.target_config import FileFormat
 from targets.values import register_value_type
 
+logger = logging.getLogger(__name__)
 
-def register_targets():
-    register_value_type(SparkDataFrameValueType())
+_DBND_REGISTER_SPARK_TYPES = None
 
-    for file_format, marshaller in (
-        (FileFormat.txt, SparkMarshaller(fmt=FileFormat.txt)),
-        (FileFormat.csv, SparkDataFrameToCsv()),
-        (FileFormat.json, SparkMarshaller(fmt=FileFormat.json)),
-        (FileFormat.parquet, SparkMarshaller(fmt=FileFormat.parquet)),
-    ):
-        register_marshaller(pyspark.sql.DataFrame, file_format, marshaller)
+
+def dbnd_register_spark_types():
+    global _DBND_REGISTER_SPARK_TYPES
+    if _DBND_REGISTER_SPARK_TYPES:
+        return
+    _DBND_REGISTER_SPARK_TYPES = True
+    try:
+        import pyspark
+    except import_errors as ex:
+        # pyspark is not installed, probably user will not use pyspark types
+        return
+
+    try:
+        register_value_type(SparkDataFrameValueType())
+
+        for file_format, marshaller in (
+            (FileFormat.txt, SparkMarshaller(fmt=FileFormat.txt)),
+            (FileFormat.csv, SparkDataFrameToCsv()),
+            (FileFormat.json, SparkMarshaller(fmt=FileFormat.json)),
+            (FileFormat.parquet, SparkMarshaller(fmt=FileFormat.parquet)),
+        ):
+            register_marshaller(pyspark.sql.DataFrame, file_format, marshaller)
+    except Exception:
+        logger.info("Failed to register pyspark types")
