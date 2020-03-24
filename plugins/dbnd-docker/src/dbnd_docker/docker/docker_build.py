7eaa7c6ce9de83242f31d9230a6b28de6f514692
Yevgeny Zaligan <yevgeny.zaligan@databand.ai>
2020-03-24 10:24:25 +0200

    Added basic support for Docker build using kaniko

--- plugins/dbnd-docker/src/dbnd_docker/docker/docker_build.py
+++ plugins/dbnd-docker/src/dbnd_docker/docker/docker_build.py
@@ -11,6 +11,8 @@ logger = logging.getLogger(__name__)
 
 
 class DockerBuild(Task):
+    use_koniko = parameter(default=True)[bool]
+
     docker_file = parameter(default=project_path("Dockerfile"))[str]
     image_name = parameter()[str]
     tag = parameter(default="latest")[str]
@@ -20,11 +22,22 @@ class DockerBuild(Task):
 
     working_dir = parameter(default=None)[str]
 
+    kaniko_command = parameter(default=None)[str]
+    context = parameter(default=None)[str]
+    destinations = parameter(default=None)[list]
+    build_args = parameter(default=None)[list]
+
     full_image_name = None
     computed_tag = None
     image_name_with_tag = None
 
     def run(self):
+        if self.use_koniko:
+            return self.run_using_kaniko()
+        else:
+            return self.run_using_docker_build()
+
+    def run_using_docker_build(self):
         if self.tag:
             self.image_name_with_tag = "{}:{}".format(self.image_name, self.tag)
         else:
@@ -61,5 +74,39 @@ class DockerBuild(Task):
                 )
         else:
             logger.info("skipping docker push")
+
+        return self.image_name_with_tag
+
+    def run_using_kaniko(self):
+        if self.tag:
+            self.image_name_with_tag = "{}:{}".format(self.image_name, self.tag)
+        else:
+            self.image_name_with_tag = self.full_image_name
+
+        command = "{} -c {} -d {} --cache=true".format(self.kaniko_command, self.context, self.docker_file)
+
+        if self.destinations is None or len(self.destinations) == 0:
+            command = command + "--no-push"
+        else:
+            destination_list = ["--destination {}".format(destination) for destination in self.destinations]
+            command = command + "".join(destination_list)
+
+        if self.build_args is not None and len(self.build_args):
+            build_args_list = ["--build-args {}".format(arg) for arg in self.build_args]
+            command = command + "".join(build_args_list)
+
+        if self.label:
+            command = command + " --label " + self.label
+
+        if self.target:
+            command = command + " --target " + self.target
+
+        try:
+            run_cmd(command, shell=True, cwd=project_path())
+        except Exception as e:
+            raise DatabandRuntimeError(
+                "failed building docker image {}".format("?"),
+                nested_exceptions=[e],
+            )
 
         return self.image_name_with_tag
