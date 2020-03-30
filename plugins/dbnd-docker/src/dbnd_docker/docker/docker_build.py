62d492e35297dec0248c4c2405d0d533fbc06f1c
Yevgeny Zaligan <yevgeny.zaligan@databand.ai>
2020-03-30 08:23:46 +0000

    Revert "Merge branch 'feature/Implement-Kaniko-support' into 'develop'"
    This reverts merge request !791

--- plugins/dbnd-docker/src/dbnd_docker/docker/docker_build.py
+++ plugins/dbnd-docker/src/dbnd_docker/docker/docker_build.py
@@ -11,8 +11,6 @@ logger = logging.getLogger(__name__)
 
 
 class DockerBuild(Task):
-    use_kaniko = parameter(default=False)[bool]
-
     docker_file = parameter(default=project_path("Dockerfile"))[str]
     image_name = parameter()[str]
     tag = parameter(default="latest")[str]
@@ -22,22 +20,11 @@ class DockerBuild(Task):
 
     working_dir = parameter(default=None)[str]
 
-    kaniko_command = parameter(default=None)[str]
-    context = parameter(default=None)[str]
-    destinations = parameter(default=None)[list]
-    build_args = parameter(default=None)[list]
-
     full_image_name = None
     computed_tag = None
     image_name_with_tag = None
 
     def run(self):
-        if self.use_kaniko:
-            return self.run_using_kaniko()
-        else:
-            return self.run_using_docker_build()
-
-    def run_using_docker_build(self):
         if self.tag:
             self.image_name_with_tag = "{}:{}".format(self.image_name, self.tag)
         else:
@@ -51,11 +38,6 @@ class DockerBuild(Task):
                 cmd = cmd + " --label " + self.label
             if self.target:
                 cmd = cmd + " --target " + self.target
-
-            if self.build_args:
-                build_args_list = [" --build-arg {}".format(arg) for arg in self.build_args]
-                cmd = cmd + "".join(build_args_list)
-
             logger.info("Running docker build: %s", cmd)
             cwd = self.working_dir or project_path()
             run_cmd(cmd, shell=True, cwd=cwd)
@@ -81,40 +63,3 @@ class DockerBuild(Task):
             logger.info("skipping docker push")
 
         return self.image_name_with_tag
-
-    def run_using_kaniko(self):
-        if self.tag:
-            self.image_name_with_tag = "{}:{}".format(self.image_name, self.tag)
-        else:
-            self.image_name_with_tag = self.full_image_name
-
-        command = "{} -c {} -f {}".format(
-            self.kaniko_command, self.context, self.docker_file
-        )
-
-        if not self.destinations:
-            command = command + " --no-push"
-        else:
-            destination_list = [
-                " -d {}".format(destination) for destination in self.destinations
-            ]
-            command = command + "".join(destination_list)
-
-        if self.build_args:
-            build_args_list = [" --build-arg {}".format(arg) for arg in self.build_args]
-            command = command + "".join(build_args_list)
-
-        if self.label:
-            command = command + " --label " + self.label
-
-        if self.target:
-            command = command + " --target " + self.target
-
-        try:
-            logger.info("Running build using Kaniko: %s", command)
-            run_cmd(command, shell=True, cwd=project_path())
-        except Exception as e:
-            raise DatabandRuntimeError(
-                "failed building docker image {}".format("?"), nested_exceptions=[e]
-            )
-
