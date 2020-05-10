65aa00ac5c9c0f6bb64c2d80d327e43a9bf70ac5
Evgeny Shulman <evgeny.shulman@databand.ai>
2020-05-10 22:47:24 +0300

    airflow operator catcher is extracted from context

--- modules/dbnd-airflow/src/dbnd_airflow/dbnd_task_executor/airflow_operators_catcher.py
+++ modules/dbnd-airflow/src/dbnd_airflow/dbnd_task_executor/airflow_operators_catcher.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 
+from dbnd_airflow.config import get_dbnd_default_args
+
 
 class DatabandOpCatcherDag(DAG):
     """
@@ -17,3 +19,23 @@ class DatabandOpCatcherDag(DAG):
         )
 
         AirflowOperatorAsDbndTask.build_airflow_task(airflow_operator=task, dag=self)
+
+
+_dag_catcher = None
+
+
+def get_databand_op_catcher_dag():
+    import airflow
+
+    if airflow.settings.CONTEXT_MANAGER_DAG:
+        # we are inside native airflow DAG or already have DatabandOpCatcherDag
+        return None
+
+    global _dag_catcher
+    if not _dag_catcher:
+        _dag_catcher = DatabandOpCatcherDag(
+            dag_id="_dbnd_airflow_operator_catcher",
+            default_args=get_dbnd_default_args(),
+        )
+    _dag_catcher.task_dict = {}
+    return _dag_catcher
