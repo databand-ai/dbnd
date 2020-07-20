77e3889dca0215c74c924713ea46fb5108f3d1d0
Yevgeny Zaligan <yevgeny.zaligan@databand.ai>
2020-07-20 10:57:14 +0300

    Fixed py2 support

--- modules/dbnd/src/dbnd/tasks/py_distribution/fat_wheel_tasks.py
+++ modules/dbnd/src/dbnd/tasks/py_distribution/fat_wheel_tasks.py
@@ -8,10 +8,12 @@ from dbnd.tasks.py_distribution.fat_wheel_builder import (
 
 
 @task
-def fat_wheel_building_task(use_cached=True, output_dir=None) -> str:
+def fat_wheel_building_task(use_cached=True, output_dir=None):
+    # type: (bool, str) -> str
     return build_fat_wheel(use_cached, output_dir)
 
 
 @task
-def wheel_zips_building_task(use_cached=True, output_dir=None) -> List[str]:
+def wheel_zips_building_task(use_cached=True, output_dir=None):
+    # type: (bool, str) -> List[str]
     return build_wheel_zips(use_cached, output_dir)
