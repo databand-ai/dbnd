a11aa9b1c89c5201ef6a8593666ab1636ca1ddc2
Yevgeny Zaligan <yevgeny.zaligan@databand.ai>
2020-07-29 12:03:49 +0000

    Revert "Fixed disable tracking api option"
    This reverts commit 3e84cb595020378dec7bed11e47d704307a4b0f3

--- modules/dbnd/src/dbnd/_core/tracking/backends/channels/tracking_disabled_channel.py
+++ modules/dbnd/src/dbnd/_core/tracking/backends/channels/tracking_disabled_channel.py
@@ -1,4 +1,5 @@
 import logging
+import pprint
 
 from dbnd._core.tracking.backends.channels.abstract_channel import TrackingChannel
 
@@ -7,12 +8,9 @@ logger = logging.getLogger(__name__)
 
 
 class DisabledTrackingChannel(TrackingChannel):
-    def __init__(self):
+    def __init__(self, print_func=None):
         super(DisabledTrackingChannel, self).__init__()
         logger.info("Tracking store is disable at core.tracker_api.")
 
     def is_ready(self):
         return True
-
-    def _handle(self, name, data):
-        pass
