c03069423e866216f14e568d28afabcc2df5b5e0
Jonathan Shir <jonathan.shir@databand.ai>
2020-09-16 18:51:36 +0300

    Bump version: 0.28.16 → 0.28.17

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -133,7 +133,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.28.16"
+__version__ = "0.28.17"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
