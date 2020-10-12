ab53af661e8eeae63fc066ce9b6f737a36a5e35a
Tomek Urbaszek <tomasz.urbaszek@polidea.com>
2020-10-12 10:58:00 +0200

    fixup! fixup! fixup! fixup! Add find_and_kill_zombies command

--- modules/dbnd-airflow/src/dbnd_airflow/scheduler/single_dag_run_job.py
+++ modules/dbnd-airflow/src/dbnd_airflow/scheduler/single_dag_run_job.py
@@ -817,8 +817,7 @@ def find_and_kill_zombies(args, session=None):
         seconds=seconds_from_last_heartbeat
     )
     logger.info(
-        "Cleaning zombie tasks with heartbeat older than: %s",
-        last_expected_heartbeat,
+        "Cleaning zombie tasks with heartbeat older than: %s", last_expected_heartbeat,
     )
 
     # Select BackfillJob that failed or are still running
