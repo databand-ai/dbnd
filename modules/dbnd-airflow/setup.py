563a2aa1565d47c7d914e1d66802df162502dd52
Filip <filip.hoffmann@polidea.com>
2020-10-19 16:25:51 +0200

    fix bugs
    * Pin Werkzeug to be above or equal v0.15.0 - earlier versions are bugged.
    * Read configuration in dbnd module setup.py - previously configuration was ignored which could result in version mismatch between dbnd-airflow and dbnd modules.
    * Fix issue in Dockerfile.dev where setup.cfg was not copied properly
    * Add packaging dependency in dbnd-airflow. This 3rd party package has already been used in this module so this could potentially lead to import error.
    * Install airflow related plugins in Dockerfile.

--- modules/dbnd-airflow/setup.py
+++ modules/dbnd-airflow/setup.py
@@ -13,7 +13,7 @@ version = config["metadata"]["version"]
 
 requirements_for_airflow = [
     "WTForms<2.3.0",  # fixing ImportError: cannot import name HTMLString at 2.3.0
-    "Werkzeug<1.0.0",
+    "Werkzeug<1.0.0,>=0.15.0",
     "cattrs==1.0.0",  # airflow requires ~0.9 but it's py2 incompatible (bug)
     "psycopg2>=2.7.4,<2.8",
 ]
@@ -21,7 +21,7 @@ requirements_for_airflow = [
 setuptools.setup(
     name="dbnd-airflow",
     package_dir={"": "src"},
-    install_requires=["dbnd==" + version],
+    install_requires=["dbnd==" + version, "packaging"],
     extras_require=dict(
         airflow=requirements_for_airflow + ["apache-airflow==1.10.7"],
         airflow_1_10_7=requirements_for_airflow + ["apache-airflow==1.10.7"],
