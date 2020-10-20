7567c9acdb7da1caa325689bd82d69c8a91cc218
FHoffmannCode <filip.hoffmann@polidea.com>
2020-10-20 09:52:15 +0200

    fix bugs (#21)
    * Pin Werkzeug to be above or equal v0.15.0 - earlier versions are bugged.
    * Read configuration in dbnd module setup.py - previously configuration was ignored which could result in version mismatch between dbnd-airflow and dbnd modules.
    * Fix issue in Dockerfile.dev where setup.cfg was not copied properly
    * Add packaging dependency in dbnd-airflow. This 3rd party package has already been used in this module so this could potentially lead to import error.
    * Install airflow related plugins in Dockerfile.

--- modules/dbnd/setup.py
+++ modules/dbnd/setup.py
@@ -1,5 +1,14 @@
+from os import path
+
 import setuptools
 
+from setuptools.config import read_configuration
+
+
+BASE_PATH = path.dirname(__file__)
+CFG_PATH = path.join(BASE_PATH, "setup.cfg")
+
+config = read_configuration(CFG_PATH)
 
 # A list of vendored packages
 dbnd_vendors_list = [
