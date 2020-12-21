badd96d8a67d44ff1b628263e60e19b60c7c81c3
Jonathan Shir <jonathan.shir@databand.ai>
2020-12-21 13:56:44 +0200

    PR Fixes

--- modules/dbnd-airflow/src/dbnd_airflow/contants.py
+++
@@ -1,7 +0,0 @@
-from distutils.version import LooseVersion
-
-import airflow
-
-
-AIRFLOW_ABOVE_10 = LooseVersion(airflow.version.version) > LooseVersion("1.10.10")
-AIRFLOW_ABOVE_9 = LooseVersion(airflow.version.version) > LooseVersion("1.10.9")
