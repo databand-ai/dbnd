b18f28f6a01fd63826acb59fdcb917e081dd65e1
Jonathan Shir <jonathan.shir@databand.ai>
2020-12-29 18:19:10 +0200

    Separated integration tests to OSS, and non-OSS

---
+++ integration-tests/spark_int_test/test_spark_run.py
@@ -0,0 +1,64 @@
+import glob
+
+import pytest
+
+from pytest import fixture
+
+import sh
+
+
+@fixture(autouse=True, scope="module")
+def setup_spark_connection():
+    cmd = sh.Command("dbnd-airflow")
+    cmd("connections", "--delete", "--conn_id", "spark_default")
+    cmd(
+        "connections",
+        "--add",
+        "--conn_id",
+        "spark_default",
+        "--conn_type",
+        "docker",
+        "--conn_host",
+        "local",
+        "--conn_extra",
+        '\'{"master":"local"}\'',
+        _truncate_exc=False,
+    )
+    sh.rm("-rf", "counters.csv", "outputs", "data", _truncate_exc=False)
+
+
+def dbnd_run_cmd(task_name):
+    # Disable web tracker because we don't use webserver in this test
+    return sh.dbnd("run", task_name, "--disable-web-tracker", _truncate_exc=False)
+
+
+class TestSparkRun(object):
+    def test_spark_inline(self):
+        output = dbnd_run_cmd("word_count_inline.word_count_inline").stderr.decode()
+        assert "Your run has been successfully executed!" in output, output
+
+    def test_spark_task_class(self):
+        output = dbnd_run_cmd("word_count.WordCountTask").stderr.decode()
+        assert "Your run has been successfully executed!" in output, output
+
+    def test_pyspark_task_class(self):
+        output = dbnd_run_cmd("word_count.WordCountPySparkTask").stderr.decode()
+        assert "Your run has been successfully executed!" in output, output
+
+    @pytest.mark.skip("Test requires different inttest environment")
+    def test_pyspark_plain_run(self):
+        cmd = sh.Command("spark-submit")
+        result = cmd(
+            "word_count_plain.py", "word_count.py", "outputs", _truncate_exc=False
+        )
+        stdout = result.stdout.decode()
+        stderr = result.stderr.decode()
+        assert "Your run has been successfully executed!" in stdout, stdout
+        assert "Running Spark" in stderr, stderr
+        files = [
+            x
+            for x in glob.glob(
+                "data/dbnd/log/*/*word_count_plain*/**/1-spark.log", recursive=True,
+            )
+        ]
+        assert len(files) == 3
