e7840a1808891ad0d0503abf6d2c5e1b71bafc92
Yevgeny Zaligan <yevgeny.zaligan@databand.ai>
2021-01-03 19:42:07 +0200

    Added more logs for api_client

--- modules/dbnd/src/dbnd/utils/api_client.py
+++ modules/dbnd/src/dbnd/utils/api_client.py
@@ -34,32 +34,39 @@ class ApiClient(object):
         self.default_headers = {"Accept": "application/json"}
 
     def _request(self, endpoint, method="GET", data=None, headers=None, query=None):
+        logger.info("Sending request to endpoint {}".format(endpoint))
         if not self.session:
+            logger.info("Session does not exist, creating new one")
             self._init_session(self.credentials)
 
         headers = dict(self.default_headers, **(headers or {}))
 
         url = urljoin(self._api_base_url, endpoint)
         try:
+            logger.info("Sending request to url {} method {}".format(url, method))
             resp = self.session.request(
                 method=method, url=url, json=data, headers=headers, params=query
             )
-        except requests.exceptions.ConnectionError:
+        except requests.exceptions.ConnectionError as ce:
+            logger.info("Got connection error while sending request: {}".format(ce))
             self.session = None
             raise
 
         if not resp.ok:
+            logger.info("Response is not ok, Raising DatabandApiError")
             raise DatabandApiError(
                 method, url, resp.status_code, resp.content.decode("utf-8")
             )
         if resp.content:
             try:
                 return resp.json()
-            except Exception:
+            except Exception as e:
+                logger.info("Failed to get resp.json(). Exception: {}".format(e))
                 return None
         return resp.json() if resp.content else None
 
     def _init_session(self, credentials):
+        logger.info("Initialising session for webserver")
         try:
             self.session = requests.session()
 
@@ -68,6 +75,7 @@ class ApiClient(object):
 
             token = credentials.get("token")
             if token:
+                logger.info("Using existing token")
                 self.default_headers["Authorization"] = "Bearer {}".format(token)
                 return
 
@@ -75,9 +83,11 @@ class ApiClient(object):
             self.session.get(urljoin(self._api_base_url, "/app"))
             csrf_token = self.session.cookies.get("dbnd_csrftoken")
             if csrf_token:
+                logger.info("Got csrg token from session")
                 self.default_headers["X-CSRFToken"] = csrf_token
 
             if "username" in credentials and "password" in credentials:
+                logger.info("Attempting to login to webserver")
                 self.api_request(
                     "auth/login", method="POST", data=credentials,
                 )
@@ -85,7 +95,10 @@ class ApiClient(object):
                 logger.warning(
                     "ApiClient._init_session: username or password is not provided"
                 )
-        except Exception:
+        except Exception as e:
+            logger.info(
+                "Exception occurred while initialising the session: {}".format(e)
+            )
             self.session = None
             raise
 
@@ -109,12 +122,17 @@ class ApiClient(object):
                 resp = self._request(
                     url, method=method, data=data, headers=headers, query=query
                 )
+                logger.info("Got response successfully!")
             except requests.ConnectionError as ex:
+                logger.info("Got ConnectionError!")
                 if failure_handler:
+                    logger.info("Using failure_handler")
                     failure_handler(ex, retry_policy, retry_number)
                 if retry_policy.should_retry(500, None, retry_number):
+                    logger.info("Using retry policy")
                     sleep(retry_policy.seconds_to_sleep(retry_number))
                     continue
+                logger.info("Raising api_connection_refused!")
                 raise api_connection_refused(self._api_base_url + url, ex)
             return resp
 
