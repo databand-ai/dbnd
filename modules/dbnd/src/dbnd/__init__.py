edf652c8c470e5fea67d9ce8a281a20c983dae17
jonny <jonathan.barda@databand.ai>
2021-01-13 13:43:30 +0200

    Bump version: 0.32.10 → 0.32.11

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -142,7 +142,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.32.10"
+__version__ = "0.32.11"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
