9a45f546849da04b98ac226455adf74f9ffd4678
Viktor Danyliuk <viktor.danyliuk@databand.ai>
2021-01-13 17:23:16 +0200

    fix: add missing MarshmallowMixin for DisabledTrackingChannel and ConsoleDebugTrackingChannel

--- modules/dbnd/src/dbnd/_core/tracking/backends/channels/tracking_disabled_channel.py
+++ modules/dbnd/src/dbnd/_core/tracking/backends/channels/tracking_disabled_channel.py
@@ -1,12 +1,13 @@
 import logging
 
 from dbnd._core.tracking.backends.channels.abstract_channel import TrackingChannel
+from dbnd._core.tracking.backends.channels.marshmallow_mixin import MarshmallowMixin
 
 
 logger = logging.getLogger(__name__)
 
 
-class DisabledTrackingChannel(TrackingChannel):
+class DisabledTrackingChannel(MarshmallowMixin, TrackingChannel):
     def __init__(self):
         super(DisabledTrackingChannel, self).__init__()
         logger.info("Tracking store is disable at core.tracker_api.")
