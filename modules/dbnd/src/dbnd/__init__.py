fddb273f0e2fdcf0cf3e9ac1c9e0393bdcdcb039
Alexander Zayats <alexander.zayats@databand.ai>
2021-02-09 12:41:41 +0200

    Bump version: 0.34.0 → 0.34.1

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -152,7 +152,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.34.0"
+__version__ = "0.34.1"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
