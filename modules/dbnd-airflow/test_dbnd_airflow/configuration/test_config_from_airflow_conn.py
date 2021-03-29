d3976ceeb3c0ca30afd5f054f1c623894d73bade
Ilan Tchenak <ilan.tchenak@databand.ai>
2021-03-29 18:44:40 +0300

    Added tests

--- modules/dbnd-airflow/test_dbnd_airflow/configuration/test_config_from_airflow_conn.py
+++ modules/dbnd-airflow/test_dbnd_airflow/configuration/test_config_from_airflow_conn.py
@@ -11,7 +11,8 @@ from dbnd_airflow.tracking.dbnd_airflow_conf import (
 
 
 DATABAND_URL = "http://databand_test_webserver"
-# TODO: Add tests for error handling
+
+# Log Messages
 CONN_FAILED_INFO_MSG = "The conn_id `{0}` isn't defined".format(
     DATABAND_AIRFLOW_CONN_ID
 )
@@ -21,7 +22,27 @@ CONN_FAILED_WARNING_MSG = "No extra config provided to {0} connection.".format(
 CONN_FAILED_ERROR_MSG = "Extra config for {0} connection, should be formated as a valid json.".format(
     DATABAND_AIRFLOW_CONN_ID
 )
-#
+
+# JSONS for Extra section in DBND Airflow connection
+VALID_EXTRA_JSON_FOR_CONNECTION = {
+    "name": "valid_dbnd_config_conn",
+    "value": json.dumps({"core": {"databand_url": DATABAND_URL}}),
+}
+
+BAD_JSONS_FOR_AIRFLOW_CONNECTION = [
+    {
+        "name": "invalid_dbnd_config_conn",
+        "value": "{NOT,{} A VALID} JSON",
+        "log_msg": CONN_FAILED_ERROR_MSG,
+    },
+    {
+        "name": "empty_dbnd_config_conn",
+        "value": "",
+        "log_msg": CONN_FAILED_WARNING_MSG,
+    },
+    {"name": "no_dbnd_conn", "value": "", "log_msg": CONN_FAILED_INFO_MSG},
+]
+
 
 logger = logging.getLogger(__name__)
 
@@ -29,45 +50,50 @@ logger = logging.getLogger(__name__)
 class TestConfigFromConnection(object):
     """ Check that setting dbnd_connection in airflow configures dbnd global config correctly."""
 
-    @property
-    def valid_extra_json_for_connection(self):
-        return json.dumps({"core": {"databand_url": DATABAND_URL}})
-
-    @pytest.fixture(scope="function", autouse=False)
-    def add_valid_dbnd_airflow_connection(self, af_session):
+    def set_dbnd_airflow_connection(self, af_session, json_for_connection):
         from airflow.models.connection import Connection
 
-        # TODO: Handle as fixture
-        existing_dbnd_conn = (
+        dbnd_config_connection = (
             af_session.query(Connection)
-            .filter(Connection.conn_id == "dbnd_config")
-            .one_or_none()
+            .filter(Connection.conn_id == DATABAND_AIRFLOW_CONN_ID)
+            .delete()
         )
-        if existing_dbnd_conn:
-            existing_dbnd_conn.extra = self.valid_extra_json_for_connection
-        else:
+        if json_for_connection["name"] != "no_dbnd_conn":
             dbnd_config_connection = Connection(
-                conn_id="dbnd_config",
+                conn_id=DATABAND_AIRFLOW_CONN_ID,
                 conn_type="HTTP",
-                extra=self.valid_extra_json_for_connection,
+                extra=json_for_connection["value"],
             )
             af_session.add(dbnd_config_connection)
 
         af_session.commit()
-        #
 
         airflow_db_url = af_session.bind.engine.url
         logging.info(
-            "dbnd_config connection added to: {airflow_db_url}".format(
-                airflow_db_url=airflow_db_url
+            "{config_name} added to: {airflow_db_url}".format(
+                config_name=json_for_connection["name"], airflow_db_url=airflow_db_url
             )
         )
 
-    @pytest.mark.usefixtures("add_valid_dbnd_airflow_connection")
-    def test_set_dbnd_config_from_valid_connection(self):
+    def test_setting_dbnd_config_from_valid_connection(self, af_session):
+        self.set_dbnd_airflow_connection(
+            af_session, json_for_connection=VALID_EXTRA_JSON_FOR_CONNECTION
+        )
 
-        logging.info("Setting config from Airflow dbnd_config connection")
         is_config_configured = set_dbnd_config_from_airflow_connections()
 
         assert is_config_configured
         assert config.get("core", "databand_url") == DATABAND_URL
+
+    @pytest.mark.parametrize("json_for_connection", BAD_JSONS_FOR_AIRFLOW_CONNECTION)
+    def test_exceptions_when_setting_config_from_airflow_connection(
+        self, json_for_connection, af_session, caplog
+    ):
+        self.set_dbnd_airflow_connection(af_session, json_for_connection)
+        logger.info("Set config based on  {0}  ".format(json_for_connection["name"]))
+        is_config_configured = set_dbnd_config_from_airflow_connections()
+
+        assert not is_config_configured
+        assert json_for_connection["log_msg"] in caplog.text
+
+        logger.info("Test Succeeded")
