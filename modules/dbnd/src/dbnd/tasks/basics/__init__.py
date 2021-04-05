9be5954e022755e76035db482519140787731489
Ilan Tchenak <ilan.tchenak@databand.ai>
2021-04-05 10:45:59 +0000

    Added sanity DAG to check Airflow system is configured for tracking.

--- modules/dbnd/src/dbnd/tasks/basics/__init__.py
+++ modules/dbnd/src/dbnd/tasks/basics/__init__.py
@@ -1,3 +1,6 @@
+from dbnd.tasks.basics.airflow_tracking_sanity import (
+    sanity_check_for_dbnd_airflow_tracking,
+)
 from dbnd.tasks.basics.sanity import dbnd_sanity_check
 from dbnd.tasks.basics.shell import bash_cmd, bash_script
 from dbnd.tasks.basics.simplest import SimplestPipeline, SimplestTask
