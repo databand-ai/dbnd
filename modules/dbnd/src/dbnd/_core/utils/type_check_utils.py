fddcb64c8eef844a34c470f8e181fde6bbac3509
Jonathan Barda <jonathan.barda@databand.ai>
2021-04-24 19:17:28 +0000

    Feature/jonny/support ecs operator

--- modules/dbnd/src/dbnd/_core/utils/type_check_utils.py
+++ modules/dbnd/src/dbnd/_core/utils/type_check_utils.py
@@ -1,6 +1,6 @@
 def is_instance_by_class_name(obj, class_name):
     # If it's metaclass
-    if type(obj).__class__ == type(type):
+    if issubclass(obj.__class__, type):
         return obj.__class__.__name__ == class_name
 
     for cls in obj.__class__.mro():
