57614d2ae279840ef0f034a6299ad9544c6d925b
Yevgeny Zaligan <yevgeny.zaligan@databand.ai>
2021-05-23 18:49:34 +0300

    Removed cattrs from setup.py in monitor

--- modules/dbnd-airflow-monitor/setup.py
+++ modules/dbnd-airflow-monitor/setup.py
@@ -30,7 +30,6 @@ setuptools.setup(
             "apache-airflow>=1.10.9,<=1.10.11",
             "sh",
             "SQLAlchemy==1.3.18",
-            "cattrs~=0.9",
         ],
         "composer": [
             "PyJWT==1.7.1",
