f67c2649c62770685258c0d30e5aa6357bc78356
Evgeny Shulman <evgeny.shulman@databand.ai>
2021-06-13 21:01:59 +0300

    test for config param of decorated task

--- modules/dbnd/test_dbnd/orchestration/parameters/test_parameter_config_obj.py
+++ modules/dbnd/test_dbnd/orchestration/parameters/test_parameter_config_obj.py
@@ -2,7 +2,7 @@ import logging
 
 import pytest
 
-from dbnd import Config, config, parameter
+from dbnd import Config, config, parameter, task
 from dbnd._core.errors import DatabandBuildError
 from dbnd.tasks.basics import SimplestTask
 
@@ -18,6 +18,11 @@ class TTaskwithConfigParam(SimplestTask):
     p_config = parameter[_TestConfig]
 
 
+@task
+def ttask_with_config_param(p_config: _TestConfig):
+    return p_config.c_value
+
+
 class TestParameterConfigObj(object):
     def test_value_config(self):
         conf = {_TestConfig.c_value: "tvalue"}
@@ -28,4 +33,10 @@ class TestParameterConfigObj(object):
     def test_value_not_defined(self):
         with pytest.raises(DatabandBuildError):
             actual = TTaskwithConfigParam(p_config="_TestConfig")
+            assert actual.p_config.c_value == "tvalue"
+
+    def test_decorator_config(self):
+        conf = {_TestConfig.c_value: "tvalue"}
+        with config(conf):
+            actual = ttask_with_config_param.task(_TestConfig())
             assert actual.p_config.c_value == "tvalue"
