88d046733b498b1bc8c1e43034644e7a16c28a14
Evgeny Shulman <evgeny.shulman@databand.ai>
2021-06-13 20:59:12 +0300

    airflow tracking policy comments/docs

--- plugins/dbnd-airflow-auto-tracking/src/dbnd_airflow_auto_tracking/dbnd_airflow_auto_tracking.py
+++ plugins/dbnd-airflow-auto-tracking/src/dbnd_airflow_auto_tracking/dbnd_airflow_auto_tracking.py
@@ -12,6 +12,13 @@ from dbnd_airflow.tracking.dbnd_airflow_handler import set_dbnd_handler
 
 
 # This code will run every time Airflow imports our plugin
+# it will change current airflow policy to include Databand policy via add_tracking_to_policy
+# -> `dbnd_airflow.tracking.dbnd_dag_tracking.track_task` will be applied on every task
+# -> `dbnd_airflow.tracking.execute_tracking.new_execute` will wrap Operator.execute
+# -> `dbnd_airflow.tracking.execute_tracking.af_tracking_context` will modify
+#     airflow operator to have "our tracking parameters"
+# -> `wrap_operator_with_tracking_info` will modify operator to have proper "ENV/params/spark.configs/jars.."
+#     so the internal part is tracked
 config = get_dbnd_project_config()
 if config.airflow_auto_tracking:
     # Wraps all Operator.execute() with DBND trackign wrapper
