882ca3062a10d5b397622795bd2614721b38e504
Vladimir Rozhkov <xpunkerx@gmail.com>
2021-07-15 12:45:58 +0300

    Trim env variables values so we won't get any type errors. Bump gradle version.

--- modules/dbnd-java/dbnd-client/src/test/java/ai/databand/config/JavaOptsTest.java
+++ modules/dbnd-java/dbnd-client/src/test/java/ai/databand/config/JavaOptsTest.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import static ai.databand.DbndPropertyNames.DBND__CORE__DATABAND_URL;
 import static ai.databand.DbndPropertyNames.DBND__LOG__PREVIEW_HEAD_BYTES;
+import static ai.databand.DbndPropertyNames.DBND__LOG__PREVIEW_TAIL_BYTES;
 import static ai.databand.DbndPropertyNames.DBND__TRACKING__VERBOSE;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -19,6 +20,7 @@ class JavaOptsTest {
         systemProps.put("dbnd.tracking.verbose", "True");
         systemProps.put("dbnd.core.databand_url", "https://tracker.databand.ai");
         systemProps.put("dbnd.log.preview_head_bytes", "100");
+        systemProps.put("dbnd.log.preview_tail_bytes", " 555 ");
 
         JavaOpts opts = new JavaOpts(systemProps);
         Map<String, String> values = opts.values();
@@ -27,8 +29,9 @@ class JavaOptsTest {
         assertThat("Wrong value", values.get(DBND__TRACKING__VERBOSE), Matchers.equalTo("True"));
         assertThat("Wrong value", values.get(DBND__CORE__DATABAND_URL), Matchers.equalTo("https://tracker.databand.ai"));
         assertThat("Wrong value", values.get(DBND__LOG__PREVIEW_HEAD_BYTES), Matchers.equalTo("100"));
+        assertThat("Wrong value", values.get(DBND__LOG__PREVIEW_TAIL_BYTES), Matchers.equalTo("555"));
 
-        assertThat("Key is present but shouldn't", values.size(), Matchers.equalTo(3));
+        assertThat("Key is present but shouldn't", values.size(), Matchers.equalTo(4));
     }
 
 }
