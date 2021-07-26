ef677c6653fa887a25402bbd99e8802d6d020a9d
Evgeny Shulman <evgeny.shulman@databand.ai>
2021-07-26 22:01:01 +0300

    better simple task (with sleep and loop)

--- modules/dbnd/src/dbnd/tasks/basics/sanity.py
+++ modules/dbnd/src/dbnd/tasks/basics/sanity.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import random
+import time
 
 from dbnd._core.task_build.dbnd_decorator import task
 from dbnd._core.tracking.metrics import log_metric
@@ -11,14 +12,29 @@ logger = logging.getLogger(__name__)
 
 @task
 def dbnd_sanity_check(check_time=datetime.datetime.now()):
-    # type: ( datetime.datetime)-> str
-    logger.info("Running Sanity Check!")
-    log_metric("Happiness Level", "High")
+    logger.info("Running Databand Task Sanity Check!")
+    log_metric("metric_check", "OK")
+
     logger.info("Your system is good to go! Enjoy Databand!")
-    return "Databand checked at %s" % check_time
+    return "Databand has been checked at %s" % check_time
 
 
 @task
-def dbnd_random_check(check_time=datetime.datetime.now(), fail_chance=0.5):
-    assert random.random() > fail_chance
-    return "Databand checked at %s" % check_time
+def dbnd_simple_task(
+    check_time=datetime.datetime.now(), fail_chance=0.0, sleep=0.0, loop=1,
+):
+    logger.info("Running dbnd_simple_task!")
+    log_metric("metric_check", "OK")
+    log_metric("metric_random_value", random.random())
+
+    for current in range(loop):
+        if current:
+            logger.info("Loop %s out of %s", current, loop)
+        if sleep:
+            logger.info("Sleeping for %s seconds", sleep)
+            time.sleep(sleep)
+
+        if random.random() < fail_chance:
+            raise Exception("Failed accordingly to fail_chance: %f" % fail_chance)
+
+    return "Databand has been checked at %s" % check_time
