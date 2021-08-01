b63b6cb84dcf48f80d1f506cc7c42f4f0df7d404
Evgeny Shulman <evgeny.shulman@databand.ai>
2021-08-01 22:31:02 +0300

    normalizing test scenarios pipelines (orchestration)

--- plugins/dbnd-test-scenarios/src/dbnd_test_scenarios/pipelines/slow_pipeline.py
+++ plugins/dbnd-test-scenarios/src/dbnd_test_scenarios/pipelines/slow_pipeline.py
@@ -1,21 +1,26 @@
+import logging
 import time
 
 from typing import List
 
 from dbnd import parameter, pipeline, task
 from dbnd._core.current import current_task
-from dbnd._core.utils.basics.nothing import NOTHING
 from targets import Target
+
+
+logger = logging.getLogger(__name__)
 
 
 @task
 def slow_task(inputs=parameter(empty_default=True)[List[Target]], sleep_sec=5):
-    time.sleep(sleep_sec)
+    for i in range(sleep_sec):
+        logger.info("Sleeping for 1 sec (%s from %s)", i, sleep_sec)
+        time.sleep(1)
     return current_task().task_id
 
 
 @pipeline
-def tasks():
+def pipeline_of_slow_tasks():
     first = slow_task(task_name="first")
     p_tasks = [slow_task(task_name=" p_%s" % i, inputs=[first]) for i in range(4)]
     last = slow_task(task_name="last", inputs=p_tasks)
