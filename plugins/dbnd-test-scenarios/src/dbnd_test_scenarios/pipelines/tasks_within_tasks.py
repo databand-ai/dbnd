0843d4cc2b8d818a969de33f49c4be797e228ebd
Evgeny Shulman <evgeny.shulman@databand.ai>
2021-08-01 22:31:02 +0300

    normalizing test scenarios pipelines (orchestration)

--- plugins/dbnd-test-scenarios/src/dbnd_test_scenarios/pipelines/tasks_within_tasks.py
+++ plugins/dbnd-test-scenarios/src/dbnd_test_scenarios/pipelines/tasks_within_tasks.py
@@ -4,6 +4,18 @@ from dbnd import pipeline, task
 
 
 @task
+def subtask(s):
+    return s + s
+
+
+@task
+def task_with_subtasks():
+    res1 = subtask("la ")
+    res2 = subtask(res1)
+    return subtask(res2)
+
+
+@task
 def task_innermost(n):
     sleep(1)
 
@@ -15,12 +27,12 @@ def task_inner(n):
 
 
 @task
-def task(n):
+def task1(n):
     sleep(1)
     task_inner(n)
 
 
 @pipeline
 def tasks_within_tasks():
-    task(1)
-    task(2)
+    task1(1)
+    task1(2)
