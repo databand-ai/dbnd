b6ee4980b8661ee4b165a75c91ff0c3c624bc6d4
Jonathan Barda <jonathan.barda@databand.ai>
2021-08-15 14:26:00 +0000

    Snowflake operation extract

--- modules/dbnd/src/targets/connections.py
+++ modules/dbnd/src/targets/connections.py
@@ -3,9 +3,9 @@ from typing import Optional
 
 def build_conn_path(conn_type, hostname=None, port=None, path=None):
     # type: (str, Optional[str], Optional[int], Optional[str]) -> str
-    return "{type}://{hostname}{port}{path}".format(
+    return "{type}://{hostname}{port}/{path}".format(
         type=conn_type,
         hostname=hostname if hostname else "",
         port=":" + str(port) if port else "",
-        path=path.rstrip("/") if path else "",
+        path=path.strip("/") if path else "",
     )
