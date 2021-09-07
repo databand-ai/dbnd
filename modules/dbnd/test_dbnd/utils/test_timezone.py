fa10289355e5704c1f506a0bfe6fc509b36f3f85
Evgeny Shulman <evgeny.shulman@databand.ai>
2021-09-07 20:50:38 +0300

    more tests and manual scenarios for different parts of the system:  * Airflow scheduler generation,  * run in run  * preview of values  * heartbeat mechanism

---
+++ modules/dbnd/test_dbnd/utils/test_timezone.py
@@ -0,0 +1,13 @@
+import datetime
+import logging
+
+from dbnd._core.utils.timezone import convert_to_utc
+
+
+class TestTimezone(object):
+    def test_convert_to_utc_with_00_00(self):
+        target = datetime.datetime.now()
+        converted_value = convert_to_utc(target)
+        converted_again = convert_to_utc(converted_value)
+        converted_again = convert_to_utc(converted_again)
+        assert converted_again
