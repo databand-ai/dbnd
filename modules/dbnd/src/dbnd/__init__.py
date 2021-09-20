1bb72ebfc9ad081612a72126644ccd5217b35aae
Tal Rumer <tal.rumer@databand.ai>
2021-09-20 16:05:43 +0000

    Bump version: 0.49.1 → 0.49.2

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -167,7 +167,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.49.1"
+__version__ = "0.49.2"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
