a53b825a914945e6b4f5009b78c96fe73b725416
Evgeny Shulman <evgeny.shulman@databand.ai>
2021-09-27 21:45:32 +0300

    dbnd-manage - better print on wait adding test for `tracker wait` command

--- modules/dbnd/src/dbnd/_core/cli/cmd_tracker.py
+++ modules/dbnd/src/dbnd/_core/cli/cmd_tracker.py
@@ -3,9 +3,8 @@ import sys
 import time
 
 from dbnd._core.context.databand_context import new_dbnd_context
-from dbnd._core.utils.timeout import wait_until
+from dbnd._core.utils.timeout import timeout
 from dbnd._vendor import click
-from dbnd._vendor.click import command
 
 
 logger = logging.getLogger(__name__)
@@ -27,8 +26,25 @@ def tracker():
 def wait(timeout):
     with new_dbnd_context(name="new_context") as dbnd_ctx:
         logger.info("Waiting {} seconds for tracker to become ready:".format(timeout))
-        is_ready = wait_until(dbnd_ctx.tracking_store.is_ready, timeout)
+
+        is_ready = _wait_for_tracking_store(dbnd_ctx, wait_timeout=timeout)
         if not is_ready:
             logger.error("Tracker is not ready after {} seconds.".format(timeout))
             sys.exit(1)
         logger.info("Tracker is ready.")
+
+
+def _wait_for_tracking_store(dbnd_ctx, wait_timeout):
+    try:
+        with timeout(wait_timeout, handler=lambda *args: None):
+            start_time = time.time()
+            while not dbnd_ctx.tracking_store.is_ready():
+                time.sleep(2)  # 2 seconds, 1 doesn't make a difference
+                logger.info(
+                    "Databand server is still not ready after %s seconds",
+                    round(time.time() - start_time),
+                )
+        return dbnd_ctx.tracking_store.is_ready()
+    except TimeoutError:
+        pass
+    return False
