4fa9b2f2897a7f9d41e7363622226295374e9620
Niv Sluzki <niv.sluzki@databand.ai>
2021-11-03 09:10:42 +0200

    Bump version: 0.51.4 → 0.51.5

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -167,7 +167,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.51.4"
+__version__ = "0.51.5"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
