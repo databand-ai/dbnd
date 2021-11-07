b100d15d0f9115134867884def759b321415b104
Michael Gelfand <michael.gelfand@databand.ai>
2021-11-07 17:05:07 +0200

    fix: capture_monitor_exception to be lazy evaluated
    capture_monitor_exception should accept configuration_service provider instead of configuration_service, do delay it's creation from import time to execution time

--- modules/dbnd-airflow-monitor/src/airflow_monitor/common/__init__.py
+++ modules/dbnd-airflow-monitor/src/airflow_monitor/common/__init__.py
@@ -1,11 +1,8 @@
 from airflow_monitor.common.airflow_data import MonitorState
-from airflow_monitor.shared.error_handler import (
-    capture_monitor_exception as _capture_monitor_exception,
-)
+from airflow_monitor.shared.error_handler import CaptureMonitorExceptionDecorator
 from airflow_monitor.tracking_service import get_servers_configuration_service
 
 
-def capture_monitor_exception(message=None):
-    return _capture_monitor_exception(
-        message=message, configuration_service=get_servers_configuration_service(),
-    )
+capture_monitor_exception = CaptureMonitorExceptionDecorator(
+    configuration_service_provider=get_servers_configuration_service
+)
