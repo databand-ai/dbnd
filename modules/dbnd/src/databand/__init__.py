6fe20b40856f51186b6efec151fe2c79f86c6988
Asaf Maor <asaf.maor@databand.ai>
2021-11-11 16:52:54 +0000

    Bump version: 0.53.2 → 0.53.3

--- modules/dbnd/src/databand/__init__.py
+++ modules/dbnd/src/databand/__init__.py
@@ -66,7 +66,7 @@ dbnd_config = config
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
 
-__version__ = "0.53.2"
+__version__ = "0.53.3"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
