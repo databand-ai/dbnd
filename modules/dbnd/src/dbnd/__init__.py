2ce74f9b5790f54f7a862149491bc2e94066ec7b
Roy Regev <roy.regev@databand.ai>
2021-12-08 15:42:12 +0000

    Bump version: 0.56.0 → 0.56.1

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -167,7 +167,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.56.0"
+__version__ = "0.56.1"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
