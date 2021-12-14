6c968c398a81350d084ee37f6ff8f9f47de4b5e6
Ilan Tchenak <ilan.tchenak@databand.ai>
2021-12-14 16:54:19 +0000

    1. Added DbndOperationColumnStats 2. Exposed columns_stats to operation api

--- modules/dbnd/src/dbnd/_core/tracking/schemas/column_stats.py
+++ modules/dbnd/src/dbnd/_core/tracking/schemas/column_stats.py
@@ -5,7 +5,8 @@ import attr
 from more_itertools import first
 
 from dbnd._core.tracking.schemas.base import ApiStrictSchema
-from dbnd._vendor.marshmallow import fields
+from dbnd._core.utils.dotdict import build_dict_from_instance_properties
+from dbnd._vendor.marshmallow import fields, post_dump, post_load
 
 
 def get_column_stats_by_col_name(
@@ -23,7 +24,7 @@ class ColumnStatsArgs:
     column_name: str = attr.ib()
     column_type: str = attr.ib()
 
-    records_count: int = attr.ib(default=None)
+    records_count: int = attr.ib()
     distinct_count: int = attr.ib(default=None)
     null_count: int = attr.ib(default=None)
 
@@ -49,6 +50,13 @@ class ColumnStatsArgs:
             return None
         return self.records_count - self.null_count
 
+    @property
+    def null_percent(self) -> Optional[float]:
+        if self.null_count is None:
+            return None
+
+        return (self.null_count / self.records_count) * 100
+
     def dump_to_stats_dict(self) -> dict:
         # Returns legacy stats dict for backward compatability support
         stats = {
@@ -71,31 +79,47 @@ class ColumnStatsArgs:
         filtered_stats = {k: v for k, v in stats.items() if v is not None}
         return {self.column_name: filtered_stats}
 
-    def as_dict(self):
-        return attr.asdict(self, filter=lambda _, value: value is not None)
+    def as_dict(self, with_property_methods: bool = True) -> dict:
+        if with_property_methods:
+            attr_dict = build_dict_from_instance_properties(self)
+        else:
+            attr_dict = attr.asdict(self)
+        return {k: v for k, v in attr_dict.items() if v is not None}
 
 
 class ColumnStatsSchema(ApiStrictSchema):
     column_name = fields.String(required=True)
     column_type = fields.String(required=True)
 
-    records_count = fields.Integer(allow_none=True)
-    distinct_count = fields.Integer(allow_none=True)
-    null_count = fields.Integer(allow_none=True)
-    non_null_count = fields.Integer(allow_none=True)
+    records_count = fields.Integer(required=True)
+    distinct_count = fields.Integer(required=False)
+    null_count = fields.Integer(required=False)
 
     # Metric for non-numeric column type
-    unique_count = fields.Integer(allow_none=True)
+    unique_count = fields.Integer(required=False)
     # Most frequent value
-    top_value = fields.Raw(allow_none=True)
-    top_freq_count = fields.Integer(allow_none=True)
+    top_value = fields.Raw(required=False)
+    top_freq_count = fields.Integer(required=False)
 
     # Metric for numeric column type
-    mean_value = fields.Float(allow_none=True)
-    min_value = fields.Float(allow_none=True)
-    max_value = fields.Float(allow_none=True)
-    std_value = fields.Float(allow_none=True)
+    mean_value = fields.Float(required=False)
+    min_value = fields.Float(required=False)
+    max_value = fields.Float(required=False)
+    std_value = fields.Float(required=False)
     # Percentiles
-    quartile_1 = fields.Float(allow_none=True)
-    quartile_2 = fields.Float(allow_none=True)
-    quartile_3 = fields.Float(allow_none=True)
+    quartile_1 = fields.Float(required=False)
+    quartile_2 = fields.Float(required=False)
+    quartile_3 = fields.Float(required=False)
+
+    # Hybrid properties - We only dump them so we don't load them back to ColumnStatsArgs when we make_object
+    # We don't save them in DB, and they are only computed in ColumnStatsArgs
+    non_null_count = fields.Integer(required=False, dump_only=True)
+    null_percent = fields.Float(required=False, dump_only=True)
+
+    @post_load
+    def make_object(self, data: dict) -> ColumnStatsArgs:
+        return ColumnStatsArgs(**data)
+
+    @post_dump
+    def dump_object(self, data: dict) -> dict:
+        return {k: v for k, v in data.items() if v is not None}
--- modules/dbnd/src/dbnd/_core/utils/dotdict.py
+++ modules/dbnd/src/dbnd/_core/utils/dotdict.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
 
@@ -8,3 +11,13 @@ class dotdict(dict):
 
 def _as_dotted_dict(**dict_obj):
     return dotdict(**dict_obj)
+
+
+def build_dict_from_instance_properties(class_instance) -> dict:
+    """Retrunes dict of all class properties including @property methods"""
+    properties = {}
+    for prop_name in dir(class_instance):
+        prop_value = getattr(class_instance, prop_name)
+        if not prop_name.startswith("__") and not inspect.ismethod(prop_value):
+            properties[prop_name] = prop_value
+    return properties
