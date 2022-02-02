82a2a63645cb5da01cae8fa7a2006f2caea2e288
Asaf Maor <asaf.maor@databand.ai>
2022-02-02 13:17:20 +0000

    Bump version: 0.62.0 → 0.62.1

--- examples/src/dbnd_examples/dbnd_spark/spark_deequ_sbt/build.sbt
+++ examples/src/dbnd_examples/dbnd_spark/spark_deequ_sbt/build.sbt
@@ -1,4 +1,4 @@
-val databandVersion = "0.62.0"
+val databandVersion = "0.62.1"
 
 ThisBuild / scalaVersion := "2.12.8"
 ThisBuild / version := databandVersion
--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -167,7 +167,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.62.0"
+__version__ = "0.62.1"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
