45c8fe796dced4dc1b723f6912056c83793cafb8
Tal Rumer <tal.rumer@databand.ai>
2022-03-17 16:37:26 +0200

    fix is goodo goodo 👍🏻

--- modules/dbnd/src/dbnd/api/shared_schemas/alerts_def_schema.py
+++ modules/dbnd/src/dbnd/api/shared_schemas/alerts_def_schema.py
@@ -24,6 +24,7 @@ class AlertDefsSchema(ApiStrictSchema):
     project_name = fields.Str(attribute="job.project.name")
     alert_on_historical_runs = fields.Bool()
     group_uid = fields.Str(allow_none=True, load_from="alert_group_uid")
+    root_group_uid = fields.Str(allow_none=True, load_from="alert_root_group_uid")
 
     uid = fields.Str(allow_none=True)
     value = fields.Str(allow_none=True)
