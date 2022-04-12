71389ade1a5b4295f8e43d2039bdbdf36c0dc244
Evgeny Shulman <evgeny.shulman@databand.ai>
2022-04-12 17:00:23 +0300

    initial version of k8s ci_cd

--- plugins/dbnd-test-scenarios/src/dbnd_test_scenarios/utils/subprocess_utils.py
+++ plugins/dbnd-test-scenarios/src/dbnd_test_scenarios/utils/subprocess_utils.py
@@ -7,27 +7,34 @@ logger = logging.getLogger(__name__)
 
 def _wrap_output(cmd, output):
     return (
-        f" **********-= Output {cmd} =-********\n"
+        f" **********  Subprocess Output '{cmd}'  \n"
         f"{output}\n"
-        f" **********-= End of Output {cmd} =-*************\n"
+        f" **********  Subprocess End of Output '{cmd}'  \n"
     )
 
 
-def run_subproces_check_output(cmd):
+def run_subproces_check_output(cmd, check=True):
     logger.info("Running cmd: %s", cmd)
+    cmd_str = cmd if isinstance(cmd, str) else subprocess.list2cmdline(cmd)
+    logger.info("Running cmd: %s", cmd_str)
     try:
-        output = (
-            subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-            .decode("utf-8")
-            .strip()
+        result = subprocess.run(
+            cmd_str,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True,
+            check=check,
         )
-        logger.info(f"Success run {cmd} :\n{_wrap_output(cmd, output)}")
+
+        output = result.stdout.decode("utf-8").strip()
+        logger.info(f"Success run {cmd_str}")
+        logger.info(f"\n\n\n{_wrap_output(cmd_str, output)}")
 
     except subprocess.CalledProcessError as ex:
         logger.error(
             "Failed to run %s\n%s",
             cmd,
-            _wrap_output(cmd, ex.output.decode("utf-8", errors="ignore")),
+            _wrap_output(cmd_str, ex.output.decode("utf-8", errors="ignore")),
         )
         raise ex
     return output
