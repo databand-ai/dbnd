f9618d90dad344fc0860df1e148b7a517bccb3cf
Yevgeny Zaligan <yevgeny.zaligan@databand.ai>
2022-04-20 01:18:32 +0300

    Fixed spark import order

--- plugins/dbnd-spark/src/dbnd_spark/__init__.py
+++ plugins/dbnd-spark/src/dbnd_spark/__init__.py
@@ -1,11 +1,22 @@
 # we should run bootstrap first! before we import any other pyspark object
-from dbnd_spark.spark import PySparkTask, SparkTask, spark_task
 from dbnd_spark.spark_bootstrap import dbnd_spark_bootstrap
-from dbnd_spark.spark_config import SparkConfig
-from dbnd_spark.spark_session import get_spark_session
 
 
 dbnd_spark_bootstrap()
 
+__all__ = []
 
-__all__ = ["PySparkTask", "SparkTask", "spark_task", "SparkConfig", "get_spark_session"]
+try:
+    from dbnd_spark.spark import PySparkTask, SparkTask, spark_task
+    from dbnd_spark.spark_config import SparkConfig
+    from dbnd_spark.spark_session import get_spark_session
+
+    __all__ += [
+        "PySparkTask",
+        "SparkTask",
+        "spark_task",
+        "SparkConfig",
+        "get_spark_session",
+    ]
+except ImportError:
+    raise
