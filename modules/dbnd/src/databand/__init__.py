be724923e312772a9e9d4bf607adb85170c80df2
Yevgeny Zaligan <yevgeny.zaligan@databand.ai>
2022-04-20 10:31:43 +0000

    Bump version: 0.70.6 → 0.70.7

--- modules/dbnd/src/databand/__init__.py
+++ modules/dbnd/src/databand/__init__.py
@@ -66,7 +66,7 @@ dbnd_config = config
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
 
-__version__ = "0.70.6"
+__version__ = "0.70.7"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
