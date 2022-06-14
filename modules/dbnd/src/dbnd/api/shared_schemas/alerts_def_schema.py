c4d73864362279a29142be00504469d2850d3e8d
Noa Atias <noa.atias@databand.ai>
2022-06-14 12:34:01 +0000

    add assigned receivers to alert def

--- modules/dbnd/src/dbnd/api/shared_schemas/alerts_def_schema.py
+++ modules/dbnd/src/dbnd/api/shared_schemas/alerts_def_schema.py
@@ -16,10 +16,7 @@ class AlertDefsSchema(ApiStrictSchema):
 
     created_at = fields.DateTime()
     scheduled_job_name = fields.Str(attribute="scheduled_job.name")
-    source_instance_name = fields.Method("get_tracking_source_name")
-    env = fields.Method("get_tracking_source_env")
     # TODO_CORE: API: Deprecate airflow_server_info
-    airflow_instance_name = fields.Method("get_tracking_source_name")
     project_id = fields.Int(attribute="job.project_id")
     project_name = fields.Str(attribute="job.project.name")
     alert_on_historical_runs = fields.Bool()
@@ -55,84 +52,9 @@ class AlertDefsSchema(ApiStrictSchema):
     # Type of MetricRule, found in dbnd_web. Used to build advanced_json
     metrics_rules = fields.List(fields.Dict(), allow_none=True)
 
-    # Used only used by the UI
-    affected_datasets = fields.List(fields.Dict(), allow_none=True, dump_only=True)
-
-    assigned_jobs = fields.Method(serialize="get_assigned_jobs", dump_only=True)
-    assigned_projects = fields.Method(serialize="get_assigned_projects", dump_only=True)
-
-    is_system = fields.Function(
-        lambda alert_def: alert_def.owner == "system", dump_only=True
-    )
-    has_auto_pipelines_alert = fields.Method("is_auto_pipelines_alert")
-
-    def is_auto_pipelines_alert(self, obj) -> bool:
-        return bool(len(obj.dbnd_auto_alert_definition))
-
-    def get_tracking_source_name(self, obj) -> str:
-        """Extracts tracking source name from alert definition. If source name is missing,
-           source type would be returned.
-           Used to fill schema fields.
-        Args:
-            obj: DbndAlertDefinition object from which to extract tracking source properties.
-        Returns:
-            str representing source name if exists otherwise source type as default name.
-        """
-        tracking_source = self._get_tracking_source_instance(obj)
-        return (
-            tracking_source.name
-            if tracking_source.name
-            else tracking_source.source_type
-        )
-
-    def get_tracking_source_env(self, obj):
-        return self._get_tracking_source_instance(obj).env
-
-    def _get_tracking_source_instance(self, obj):
-        if obj.job:
-            return obj.job.tracking_source
-
-        return obj.tracking_source
-
     @pre_load
     def prepere(self, data: dict, **kwargs):
         value = data.get("value", None)
         if value is not None:
             data["value"] = str(data["value"])
         return data
-
-    def get_assigned_jobs(self, alert_def):
-        if alert_def.job:
-            alert_jobs = {(alert_def.job_id, alert_def.job_name)}
-        else:
-            alert_jobs = {
-                (sub_alert.job_id, sub_alert.job_name) if sub_alert.job else None
-                for sub_alert in alert_def.sub_alert_definitions
-            }
-        alert_jobs = filter(None, alert_jobs)
-        serialized_assigned_jobs = (
-            [
-                {"job_id": job_id, "job_name": job_name}
-                for job_id, job_name in alert_jobs
-            ]
-            if alert_jobs
-            else []
-        )
-        return serialized_assigned_jobs
-
-    def get_assigned_projects(self, alert_def):
-        if alert_def.job:
-            alert_jobs = {(alert_def.job.project.name, alert_def.job.project.id)}
-        else:
-            alert_jobs = {
-                (sub_alert.job.project.name, sub_alert.job.project.id)
-                if sub_alert.job
-                else None
-                for sub_alert in alert_def.sub_alert_definitions
-            }
-        alert_jobs = set(filter(None, alert_jobs))
-        serialized_assigned_projects = [
-            {"project_name": project_name, "project_id": project_id}
-            for project_name, project_id in alert_jobs
-        ]
-        return serialized_assigned_projects
