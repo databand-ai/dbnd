a6c5255834ef333cb51abe8a1436ccde4c278cea
Niv Sluzki <niv.sluzki@databand.ai>
2022-06-16 10:04:43 +0000

    Bump version: 0.75.14 → 0.75.15

--- examples/src/dbnd_examples/dbnd_spark/spark_deequ_sbt/build.sbt
+++ examples/src/dbnd_examples/dbnd_spark/spark_deequ_sbt/build.sbt
@@ -1,4 +1,4 @@
-val databandVersion = "0.75.14"
+val databandVersion = "0.75.15"
 
 ThisBuild / scalaVersion := "2.12.8"
 ThisBuild / version := databandVersion
--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -174,7 +174,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.75.14"
+__version__ = "0.75.15"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
