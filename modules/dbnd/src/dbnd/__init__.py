8835d9d9859618910478615446c9bfaa667f1703
Niv Sluzki <niv.sluzki@databand.ai>
2022-08-16 17:07:39 +0000

    Bump version: 0.86.0 → 0.86.1

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -176,7 +176,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.86.0"
+__version__ = "0.86.1"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
