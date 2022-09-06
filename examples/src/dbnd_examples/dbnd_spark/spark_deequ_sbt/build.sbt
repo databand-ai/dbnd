68ede96d9f3b8cf15a17cf6ed3ec450edf3deb68
Michael Gelfand <michael.gelfand@gmail.com>
2022-09-06 08:58:44 +0000

    Bump version: 0.89.9 → 0.89.10

--- examples/src/dbnd_examples/dbnd_spark/spark_deequ_sbt/build.sbt
+++ examples/src/dbnd_examples/dbnd_spark/spark_deequ_sbt/build.sbt
@@ -1,4 +1,4 @@
-val databandVersion = "0.89.9"
+val databandVersion = "0.89.10"
 
 ThisBuild / scalaVersion := "2.12.8"
 ThisBuild / version := databandVersion
--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -176,7 +176,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "0.89.9"
+__version__ = "0.89.10"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
