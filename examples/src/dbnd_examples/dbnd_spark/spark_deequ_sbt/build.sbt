2efde514d3602a4dbaf0c798c5b07794822e2c10
Vova Rozhkov <xpunkerx@gmail.com>
2022-11-09 14:15:33 +0000

    Bump version: 1.0.6.0 → 1.0.7.0

--- examples/src/dbnd_examples/dbnd_spark/spark_deequ_sbt/build.sbt
+++ examples/src/dbnd_examples/dbnd_spark/spark_deequ_sbt/build.sbt
@@ -1,4 +1,4 @@
-val databandVersion = "1.0.6.0"
+val databandVersion = "1.0.7.0"
 
 ThisBuild / scalaVersion := "2.12.8"
 ThisBuild / version := databandVersion
--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -176,7 +176,7 @@ __all__ = [
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "1.0.6.0"
+__version__ = "1.0.7.0"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
