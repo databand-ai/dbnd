cdd9e13e84e6f250f96f967bf270f65ccc2355ab
Michael Gelfand <michael.gelfand@gmail.com>
2022-12-25 09:57:44 +0000

    chore: fix to support python 3.10 for web [MR!7505]

--- modules/dbnd/setup.py
+++ modules/dbnd/setup.py
@@ -27,7 +27,7 @@ dbnd_vendors_list = [
 setuptools.setup(
     name="dbnd",
     package_dir={"": "src"},
-    python_requires=">=3.6, <=3.10",
+    python_requires=">=3.6, <3.11",
     install_requires=[
         "tzlocal",
         "six",
