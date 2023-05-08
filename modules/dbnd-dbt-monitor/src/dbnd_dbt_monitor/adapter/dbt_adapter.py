53a09ebdf3a68d5484d6de1e300351b8f9e41beb
Roni Shternbach <roni.shternbach@databand.ai>
2023-05-08 11:16:55 +0000

    dbt init assets for cursor [MR!8382]

--- modules/dbnd-dbt-monitor/src/dbnd_dbt_monitor/adapter/dbt_adapter.py
+++ modules/dbnd-dbt-monitor/src/dbnd_dbt_monitor/adapter/dbt_adapter.py
@@ -4,7 +4,12 @@ from typing import Generator, Tuple
 
 from dbnd_dbt_monitor.fetcher.dbt_cloud_data_fetcher import DbtCloudDataFetcher
 
-from airflow_monitor.shared.adapter.adapter import Adapter, Assets
+from airflow_monitor.shared.adapter.adapter import (
+    Adapter,
+    Assets,
+    AssetState,
+    AssetToState,
+)
 
 
 class DbtAdapter(Adapter):
@@ -17,7 +22,16 @@ class DbtAdapter(Adapter):
     def init_assets_for_cursor(
         self, cursor: object, batch_size: int
     ) -> Generator[Tuple[Assets, str], None, None]:
-        pass
+        # batch_size is configured in data_fetcher from server_config (default value=30)
+        new_dbt_run_ids = self.data_fetcher.get_run_ids_to_sync_from_dbt(cursor)
+        dbt_assets_to_states = []
+        if new_dbt_run_ids:
+            cursor = max(new_dbt_run_ids)
+            for dbt_run_id in new_dbt_run_ids:
+                dbt_assets_to_states.append(
+                    AssetToState(asset_id=str(dbt_run_id), state=AssetState.INIT)
+                )
+        yield (Assets(assets_to_state=dbt_assets_to_states), cursor)
 
     def get_assets_data(self, assets: Assets, next_page: object) -> Assets:
         pass
--- modules/dbnd-dbt-monitor/test_dbt_monitor/test_dbt_adapter.py
+++ modules/dbnd-dbt-monitor/test_dbt_monitor/test_dbt_adapter.py
@@ -4,14 +4,21 @@ import pytest
 
 from dbnd_dbt_monitor.adapter.dbt_adapter import DbtAdapter
 
+from airflow_monitor.shared.adapter.adapter import AssetState, AssetToState
+
 from .test_dbt_runs_syncer import FINISHED_STATE, MockDbtFetcher, MockDbtRun
+
+
+def generate_mock_dbt_runs(num):
+    for i in range(num):
+        yield MockDbtRun(id=i, state=FINISHED_STATE)
 
 
 @pytest.fixture
 def mock_dbt_data_fetcher() -> MockDbtFetcher:
-    old_dbt_run = MockDbtRun(id=1, state=FINISHED_STATE)
     mock_data_fetcher = MockDbtFetcher()
-    mock_data_fetcher.dbt_runs.append(old_dbt_run)
+    for mock_dbt_run in generate_mock_dbt_runs(10):
+        mock_data_fetcher.dbt_runs.append(mock_dbt_run)
     yield mock_data_fetcher
 
 
@@ -22,4 +29,23 @@ def dbt_adapter(mock_dbt_data_fetcher) -> DbtAdapter:
 
 class TestDbtAdapter:
     def test_dbt_adapter_init_cursor(self, dbt_adapter: DbtAdapter):
-        assert dbt_adapter.init_cursor() == 1
+        assert dbt_adapter.init_cursor() == 9
+
+    def test_dbt_adapter_init_assets_for_cursor(self, dbt_adapter: DbtAdapter):
+        for result_assets, next_cursor in dbt_adapter.init_assets_for_cursor(
+            cursor=5, batch_size=200
+        ):
+            assert result_assets.assets_to_state == [
+                AssetToState(asset_id="6", state=AssetState.INIT, retry_count=0),
+                AssetToState(asset_id="7", state=AssetState.INIT, retry_count=0),
+                AssetToState(asset_id="8", state=AssetState.INIT, retry_count=0),
+                AssetToState(asset_id="9", state=AssetState.INIT, retry_count=0),
+            ]
+            assert next_cursor == 9
+
+    def test_dbt_adapter_init_assets_for_last_cursor(self, dbt_adapter: DbtAdapter):
+        for result_assets, next_cursor in dbt_adapter.init_assets_for_cursor(
+            cursor=9, batch_size=200
+        ):
+            assert result_assets.assets_to_state == []
+            assert next_cursor == 9
