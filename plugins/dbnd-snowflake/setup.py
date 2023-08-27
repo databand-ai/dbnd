116c8c7a89809dd7be5acda10240ff3d27338eee
Evgeny Shulman <evgeny.shulman@databand.ai>
2023-08-27 15:08:29 +0300

    update staging spark and tracking scenarios with Airflow 2.3.4 support and python 3.10 [MR!9312]

--- plugins/dbnd-snowflake/setup.py
+++ plugins/dbnd-snowflake/setup.py
@@ -16,19 +16,14 @@ version = config["metadata"]["version"]
 setuptools.setup(
     name="dbnd-snowflake",
     package_dir={"": "src"},
-    install_requires=[
-        "sqlparse",
-        "snowflake-connector-python",
-        "pandas",
-        "numpy",
-        "dbnd==" + version,
-    ],
+    install_requires=["sqlparse", "dbnd==" + version],
     extras_require={
         "tests": [
             'pandas==1.3.5;python_version<"3.8"',  # M1 is supported
             'pandas==1.4.0;python_version>="3.8"',
             'numpy==1.21.6;python_version<"3.8"',  # M1 is supported
             'numpy==1.22.4;python_version>="3.8"',
+            "snowflake-connector-python",
         ]
     },
 )
