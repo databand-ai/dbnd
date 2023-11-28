be879d78eced2105889d276979f295ebb3edfd9c
Dbnd-devops-ghe <dbnd-devops-ghe@ibm.com>
2023-11-28 13:34:17 +0000

    Bump version: 1.0.18.0 → 1.0.19.0

--- modules/dbnd/src/dbnd/__init__.py
+++ modules/dbnd/src/dbnd/__init__.py
@@ -194,7 +194,7 @@ if is_dbnd_run_package_installed():
 
 # shortcuts for useful objects
 str(_set_patches)  # NOQA
-__version__ = "1.0.18.0"
+__version__ = "1.0.19.0"
 
 __title__ = "databand"
 __description__ = "Machine Learning Orchestration"
