25eb794c0f77c311c4a5016b9d64c43ae5e57dea
Evgeny Shulman <evgeny.shulman@databand.ai>
2023-12-03 14:41:59 +0200

    fix task_engine access from k8s context

--- orchestration/dbnd-run/src/dbnd_run/airflow/dbnd_task_executor/dbnd_execute.py
+++ orchestration/dbnd-run/src/dbnd_run/airflow/dbnd_task_executor/dbnd_execute.py
@@ -145,9 +145,12 @@ def dbnd_operator__get_task_retry_delay(dbnd_operator):
 
     task_run = run.get_task_run_by_id(dbnd_operator.dbnd_task_id)
 
-    if task_run.task_engine.task_definition.task_family == "kubernetes":
+    if (
+        task_run.task_run_executor.task_engine.task_definition.task_family
+        == "kubernetes"
+    ):
         # If we are running in K8s - use pod retry delay instead of task retry delay
 
-        return task_run.task_engine.pod_default_retry_delay
+        return task_run.task_run_executor.pod_default_retry_delay
     else:
         return task_run.task.task_retry_delay
