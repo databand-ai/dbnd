9cb28a9e3a65fe17ef80b036a7c932cc759abc49
Michael Gelfand <Michael.Gelfand@ibm.com>
2024-07-08 16:32:13 +0300

    Michael/feat/instana monitoring (#1288)
    * use instana for detailed tracking
    
    * remove newrelic from requirements
    
    * add entry span tag to apm_track_function
    
    * fixes
    
    * remove stopit usage
    
    * typing fix/pre-commit
    
    * extracted apm_start_active_span

--- modules/dbnd-airflow-monitor/src/airflow_monitor/shared/monitoring/apm.py
+++ modules/dbnd-airflow-monitor/src/airflow_monitor/shared/monitoring/apm.py
@@ -3,69 +3,72 @@
 import contextlib
 import logging
 import os
-import sys
+
+from functools import wraps
 
 
 logger = logging.getLogger(__name__)
 
+instana_available = False
 try:
-    import newrelic.agent
+    if os.environ.get("INSTANA_ENABLED", "false").lower() == "true":
+        from instana.singletons import tracer
 
-    newrelic_available = True
+        instana_available = True
 except ImportError:
-    newrelic_available = False
+    pass
 
 
 def configure_apm():
-    if os.environ.get("INSTANA_ENABLED", "false").lower() == "true":
-        try:
-            import instana  # noqa: F401 pylint: disable=unused-import
-        except ImportError:
-            pass
-
-    configure_newrelic()
-
-
-def configure_newrelic():
-    if not newrelic_available:
+    if not instana_available:
         return
 
-    from newrelic.core.config import apply_config_setting, global_settings
-
-    ignore_errors = (
-        "google.auth.exceptions:RefreshError",
-        "requests.exceptions:ConnectionError",
-        "requests.exceptions:HTTPError",
-        "requests.exceptions:SSLError",
-        "requests.exceptions:Timeout",
-        "requests.exceptions:ConnectTimeout",
-        "airflow_monitor.shared.errors:ClientConnectionError",
-        "dbnd._core.errors.base:DatabandConnectionException",
-    )
-    apply_config_setting(
-        global_settings(), "error_collector.ignore_classes", " ".join(ignore_errors)
-    )
+    # ignore_errors = (
+    #     "google.auth.exceptions:RefreshError",
+    #     "requests.exceptions:ConnectionError",
+    #     "requests.exceptions:HTTPError",
+    #     "requests.exceptions:SSLError",
+    #     "requests.exceptions:Timeout",
+    #     "requests.exceptions:ConnectTimeout",
+    #     "airflow_monitor.shared.errors:ClientConnectionError",
+    #     "dbnd._core.errors.base:DatabandConnectionException",
+    # )
+    # apply_config_setting(
+    #     global_settings(), "error_collector.ignore_classes", " ".join(ignore_errors)
+    # )
 
 
 @contextlib.contextmanager
 def transaction_scope(name: str):
-    if not newrelic_available:
+    if not instana_available:
         yield
         return
 
     try:
-        background_task = newrelic.agent.BackgroundTask(
-            application=newrelic.agent.application(),
-            name=name,
-            group=sys.argv[0].rsplit("/", 1)[-1],
-        )
+        tags = {"span.kind": "entry"} if not tracer.active_span else {}
+        active_span = tracer.start_active_span(name, tags=tags)
     except Exception:
-        logger.warning("Error while creating newrelic background_task", exc_info=True)
-        background_task = None
+        logger.warning("Error while starting instana active_span", exc_info=True)
+        active_span = None
 
-    if not background_task:
+    if not active_span:
         yield
         return
 
-    with background_task:
+    with active_span:
         yield
+
+
+def apm_track_function():
+    def wrapper(f):
+        if not instana_available:
+            return f
+
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            with transaction_scope(f"{f.__module__}.{f.__qualname__}"):
+                return f(*args, **kwargs)
+
+        return wrapped
+
+    return wrapper
