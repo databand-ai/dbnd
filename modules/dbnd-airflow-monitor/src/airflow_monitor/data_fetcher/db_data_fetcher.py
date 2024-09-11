db46663b586428917c7bfe0ed278fdc9aa594dca
Filip Olszowski <Filip.Olszowski@ibm.com>
2024-09-11 14:03:04 +0200

    Add the ability to store and retrieve a unique UUID in Airflow (#1693)

--- modules/dbnd-airflow-monitor/src/airflow_monitor/data_fetcher/db_data_fetcher.py
+++ modules/dbnd-airflow-monitor/src/airflow_monitor/data_fetcher/db_data_fetcher.py
@@ -4,7 +4,7 @@ import contextlib
 import json
 import logging
 
-from typing import List, Optional
+from typing import TYPE_CHECKING, List, Optional
 
 from airflow_monitor.common.airflow_data import (
     AirflowDagRunsResponse,
@@ -19,6 +19,10 @@ from airflow_monitor.errors import AirflowFetchingException
 from dbnd._vendor.tenacity import retry, stop_after_attempt, wait_fixed
 
 
+if TYPE_CHECKING:
+    from airflow.models import Variable  # noqa: F401
+
+AIRFLOW_INSTANCE_UUID_VAR_NAME = "DBND_AIRFLOW_INSTANCE_UUID"
 logger = logging.getLogger(__name__)
 
 
@@ -37,6 +41,7 @@ class DbFetcher(AirflowDataFetcher):
         # It's important to do this import to prevent import issues
         import airflow  # noqa: F401
 
+        from airflow.models import Variable  # noqa: F401
         from sqlalchemy import create_engine
 
         from dbnd_airflow.export_plugin.smart_dagbag import DbndDagLoader
@@ -180,3 +185,9 @@ class DbFetcher(AirflowDataFetcher):
 
     def is_alive(self):
         return True
+
+    def get_airflow_instance_uid(self) -> Optional[str]:
+        return Variable.get(AIRFLOW_INSTANCE_UUID_VAR_NAME, default_var=None)
+
+    def set_airflow_instance_uid(self, new_airflow_instance_uid: str) -> None:
+        Variable.set(AIRFLOW_INSTANCE_UUID_VAR_NAME, new_airflow_instance_uid)
