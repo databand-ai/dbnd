8092ead028344b5de7645612c07cb458a43daf32
Yevgeny Zaligan <Yevgeny.Zaligan@ibm.com>
2024-10-28 12:27:29 +0200

    Fixed version parsing using setuptools (#2046)
    * Fixed version parsing using setuptools
    
    * Fixed

--- modules/dbnd/src/dbnd/_vendor/version.py
+++ modules/dbnd/src/dbnd/_vendor/version.py
@@ -1,19 +1,19 @@
 
 
 
-def parse(version):
+def parse(version_to_parse):
     """
     Parse the given version string
     If setuptools are available, it will return Version/LegacyVersion, otherwise LegacyVersion from distutils
     """
     try:
-        from setuptools.extern.packaging.version import parse as setuptools_parse
-        return setuptools_parse(version)
+        from packaging.version import parse as packaging_parse
+        return packaging_parse(version_to_parse)
     except ImportError:
         try:
             # setuptools is an optional package. although distutils is going to be deprecated in py3.12+
             from distutils import version
-            return version.LooseVersion(version)
+            return version.LooseVersion(version_to_parse)
         except ImportError:
             raise Exception("distutils and setuptools are not installed, "
                             "please install one of them, or de-install `dbnd` package")
