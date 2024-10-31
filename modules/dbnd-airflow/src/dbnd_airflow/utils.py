cc09976b7c9160d0eafd5b444afb795d1980ebc7
Filip Olszowski <Filip.Olszowski@ibm.com>
2024-10-31 13:34:07 +0100

    Fix problem with changing airflow source_instatnce_uid during SDK upgrade (#2068)
    Restore original logic for Airflow source_instance_uid creation in the new flow (storing it in Airflow Variable)

--- modules/dbnd-airflow/src/dbnd_airflow/utils.py
+++ modules/dbnd-airflow/src/dbnd_airflow/utils.py
@@ -1,6 +1,6 @@
 # © Copyright Databand.ai, an IBM Company 2022
 
-from uuid import uuid4
+from uuid import NAMESPACE_URL, uuid5
 
 from dbnd._core.utils.uid_utils import get_job_run_uid, get_task_run_attempt_uid
 
@@ -19,7 +19,13 @@ def get_task_run_attempt_uid_from_af_ti(ti):
 
 
 def get_or_create_airflow_instance_uid() -> str:
-    """used to distinguish between jobs of different airflow instances"""
+    """
+    Used to distinguish between jobs of different airflow instances.
+    airflow_instance_uid is stored in "DBND_AIRFLOW_INSTANCE_UUID" Airflow Variable.
+    It's generated from DB connection string for backwards compatibility.
+    """
+    import airflow  # pylint: disable=import-error
+
     from airflow.models import Variable  # pylint: disable=import-error
 
     airflow_instance_uid = Variable.get(
@@ -28,6 +34,9 @@ def get_or_create_airflow_instance_uid() -> str:
     if airflow_instance_uid:
         return airflow_instance_uid
 
-    new_airflow_instance_uid = str(uuid4())
+    db_url = airflow.settings.Session.bind.engine.url
+    db_str = f"{db_url.host}:{db_url.port}/{db_url.database}"
+    new_airflow_instance_uid = str(uuid5(NAMESPACE_URL, db_str))
+
     Variable.set(AIRFLOW_INSTANCE_UUID_VAR_NAME, new_airflow_instance_uid)
     return new_airflow_instance_uid
