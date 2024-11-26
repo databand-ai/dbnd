5e85e3da179e59e4e4a7c49d4e7a5ab7065aba99
Iurii Likh <Iurii.Likh1@ibm.com>
2024-11-26 21:45:38 +0100

    Added ComponentErrors feat/likh/ce_dto_changes (#2141)
    * Added ComponentErrors and adjusted tests
    
    * Added 'external_id' field to the ComponentErrors
    
    * Revised after a review
    
    - changed class name
    - added explanation in tests
    
    * Sligtly adjusted doc-string
    
    * Fixed type hint
    
    * Added a test condition
    
    * Typo fixed

--- modules/dbnd-monitor/src/dbnd_monitor/component_error.py
+++ modules/dbnd-monitor/src/dbnd_monitor/component_error.py
@@ -2,24 +2,27 @@
 
 from datetime import datetime
 from traceback import format_exception
+from typing import Optional
 
 from pydantic import BaseModel
+
+from dbnd._core.utils.timezone import utcnow
 
 
 class ComponentError(BaseModel):
     exception_type: str  # Exception type
-    exception_message: str  # Exception message
-    exception_traceback: str  # Stack traceback
+    exception_body: str  # Exception message
+    traceback: str  # Stack traceback
     timestamp: datetime  # UTC datetime
 
     def __hash__(self) -> int:
-        return hash(self.exception_traceback)
+        return hash(self.traceback)
 
     def __str__(self):
-        timestamp = datetime.strftime(self.timestamp, "%Y-%m-%d %H:%M:%S")
+        timestamp = self.timestamp.isoformat()
         return (
-            f"[{timestamp}] {self.exception_type}: {self.exception_message}\n"
-            f"<<<\n{self.exception_traceback}>>>"
+            f"[{timestamp}] {self.exception_type}: {self.exception_body}\n"
+            f"<<<\n{self.traceback}>>>"
         )
 
     @classmethod
@@ -30,7 +33,15 @@ class ComponentError(BaseModel):
         tb = "".join(format_exception(type(exc), exc, exc.__traceback__))
         return cls(
             exception_type=type(exc).__name__,
-            exception_message=str(exc),
-            exception_traceback=tb,
-            timestamp=datetime.now(),
+            exception_body=str(exc),
+            traceback=tb,
+            timestamp=utcnow(),
         )
+
+
+class ReportErrorsDTO(BaseModel):
+    tracking_source_uid: str
+    external_id: Optional[str]
+    component: str
+    errors: list[ComponentError] = []
+    is_error: bool = True
--- modules/dbnd-monitor/test_dbnd_monitor/test_component_error.py
+++ modules/dbnd-monitor/test_dbnd_monitor/test_component_error.py
@@ -1,38 +1,98 @@
 # © Copyright Databand.ai, an IBM Company 2024
 
-import re
+from random import choice
 
-from datetime import datetime
+import pytest
 
-import freezegun
-
-from dbnd_monitor.component_error import ComponentError
+from dbnd_monitor.component_error import ComponentError, ReportErrorsDTO
 
 
-@freezegun.freeze_time("2021-01-01 12:00:00")
-def test_component_error_from_exception():
+def test_component_error_model_dump():
     """
-    The test proves the correctness of an exception conversion
-    to a ComponentError object.
+    The test proves the equality of instances created manually and recreated
+    from the dictionary representing the ComponentError model, what means
+    serialization/deserialization works as expected
+    """
+    # Arrange:
+    # create the refference instance
+    component_error = ComponentError.from_exception(ValueError("0: Error"))
+
+    # Act:
+    # create the dictionary representing the model, i.e. model's dump
+    dumped = component_error.model_dump()
+
+    # Assert:
+    # the new instance of the ComponentError created from the model's dump
+    # must be equal to the original instance
+    assert ComponentError(**dumped) == component_error
+
+
+@pytest.mark.parametrize(
+    "param",
+    [
+        pytest.param(0, id="int"),
+        pytest.param("ValueError('0: Error')", id="str"),
+        pytest.param((ValueError, "0: Error"), id="tuple"),
+    ],
+)
+def test_component_error_raises_value_error(param):
+    """
+    The test proves that the `from_exception` method raises a ValueError
+    when the input is not an exception instance
+    """
+    with pytest.raises(ValueError) as value_error:
+        ComponentError.from_exception(param)
+
+    assert value_error.match("Expected an exception instance")
+
+
+def test_report_errors_dto_model_dump():
+    """
+    The test proves the equality of instances created manually and recreated
+    from the dictionary representing the ReportErrorDTO model, what means
+    serialization/deserialization works as expected
     """
 
-    try:
-        raise ValueError("Test error")
-    except Exception as exc:
-        result = ComponentError.from_exception(exc)
+    external_id = choice(["test_external_id", None])
+    error_0 = ComponentError.from_exception(ValueError("0: Error"))
+    error_1 = ComponentError.from_exception(AttributeError("1: Error"))
 
-        assert result.exception_type == "ValueError"
-        assert result.exception_message == "Test error"
-        assert all(
-            text in result.exception_traceback
-            for text in [
-                "test_component_error.py",
-                "line 20, in test_component_error_from_exception",
-                "ValueError: Test error",
-            ]
-        )
-        assert result.timestamp == datetime(2021, 1, 1, 12, 0, 0)
+    # Arrange:
+    # create the refference instance
+    report_errors_dto = ReportErrorsDTO(
+        tracking_source_uid="test_tracking_source_uid",
+        external_id=external_id,
+        component="test_component",
+        errors=[error_0, error_1],
+    )
 
-        assert re.match(
-            r"\[\d{4}-\d{2}-\d{2} \d{2}\:\d{2}\:\d{2}\] (.+): (.+)", str(result)
-        )
+    # Act:
+    # create the dictionary representing the model, i.e. model's dump
+    dumped = report_errors_dto.model_dump()
+
+    # Assert:
+    # the new instance of the ReportErrorsDTO created from the model's dump
+    # must be equal to the original instance
+    assert ReportErrorsDTO(**dumped) == report_errors_dto
+
+    # Fully populated ReportErrorsDTO model
+    assert dumped == {
+        "tracking_source_uid": "test_tracking_source_uid",
+        "external_id": external_id,
+        "component": "test_component",
+        "errors": [
+            {
+                "exception_type": "ValueError",
+                "exception_body": "0: Error",
+                "traceback": "ValueError: 0: Error\n",
+                "timestamp": error_0.timestamp,
+            },
+            {
+                "exception_type": "AttributeError",
+                "exception_body": "1: Error",
+                "traceback": "AttributeError: 1: Error\n",
+                "timestamp": error_1.timestamp,
+            },
+        ],
+        "is_error": True,
+    }
